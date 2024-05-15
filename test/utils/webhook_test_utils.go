af5a6e4eaf898051c1e9dfde849711b19995667a
Bastian Krol <bastian.krol@dash0.com>
2024-05-15 11:06:38 +0200

    test(webhook): add cases for multiple containers, volumes etc.

--- test/utils/webhook_test_utils.go
+++ test/utils/webhook_test_utils.go
@@ -21,7 +21,7 @@ const (
 	DeploymentNameExisting = "existing-deployment"
 )
 
-func CreateDeployment(namespace string, name string) *appsv1.Deployment {
+func CreateBasicDeployment(namespace string, name string) *appsv1.Deployment {
 	deployment := &appsv1.Deployment{}
 	deployment.Namespace = namespace
 	deployment.Name = name
@@ -29,11 +29,192 @@ func CreateDeployment(namespace string, name string) *appsv1.Deployment {
 	deployment.Spec.Template = corev1.PodTemplateSpec{}
 	deployment.Spec.Template.Labels = map[string]string{"app": "test"}
 	deployment.Spec.Template.Spec.Containers = []corev1.Container{{
-		Name:  "test-container",
+		Name:  "test-container-0",
 		Image: "ubuntu",
 	}}
 	deployment.Spec.Selector = &metav1.LabelSelector{}
 	deployment.Spec.Selector.MatchLabels = map[string]string{"app": "test"}
+	return deployment
+}
+
+func CreateDeploymentWithMoreBellsAndWhistles(namespace string, name string) *appsv1.Deployment {
+	deployment := CreateBasicDeployment(namespace, name)
+	podSpec := &deployment.Spec.Template.Spec
+	podSpec.Volumes = []corev1.Volume{
+		{
+			Name:         "test-volume-0",
+			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+		},
+		{
+			Name:         "test-volume-1",
+			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+		},
+	}
+	podSpec.InitContainers = []corev1.Container{
+		{
+			Name:  "test-init-container-0",
+			Image: "ubuntu",
+			Env: []corev1.EnvVar{{
+				Name:  "TEST_INIT_0",
+				Value: "value",
+			}},
+		},
+		{
+			Name:  "test-init-container-1",
+			Image: "ubuntu",
+			Env: []corev1.EnvVar{
+				{
+					Name:  "TEST_INIT_0",
+					Value: "value",
+				},
+				{
+					Name:  "TEST_INIT_1",
+					Value: "value",
+				},
+			},
+		},
+	}
+	podSpec.Containers = append(
+		deployment.Spec.Template.Spec.Containers,
+		corev1.Container{
+			Name:  "test-container-1",
+			Image: "ubuntu",
+			VolumeMounts: []corev1.VolumeMount{{
+				Name:      "test-volume-0",
+				MountPath: "/test-1",
+			}},
+			Env: []corev1.EnvVar{{
+				Name:  "TEST0",
+				Value: "value",
+			}},
+		},
+		corev1.Container{
+			Name:  "test-container-2",
+			Image: "ubuntu",
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "test-volume-0",
+					MountPath: "/test-0",
+				},
+				{
+					Name:      "test-volume-1",
+					MountPath: "/test-1",
+				},
+			},
+			Env: []corev1.EnvVar{
+				{
+					Name:  "TEST0",
+					Value: "value",
+				},
+				{
+					Name:      "TEST1",
+					ValueFrom: &corev1.EnvVarSource{FieldRef: &corev1.ObjectFieldSelector{FieldPath: "metadata.namespace"}},
+				},
+			},
+		})
+
+	return deployment
+}
+
+func CreateDeploymentWithExistingDash0Artifacts(namespace string, name string) *appsv1.Deployment {
+	deployment := CreateBasicDeployment(namespace, name)
+	podSpec := &deployment.Spec.Template.Spec
+	podSpec.Volumes = []corev1.Volume{
+		{
+			Name:         "test-volume-0",
+			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+		},
+		{
+			Name: "dash0-instrumentation",
+			// the volume source should be updated/overwritten by the webhook
+			VolumeSource: corev1.VolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/foo/bar"}},
+		},
+		{
+			Name:         "test-volume-2",
+			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}},
+		},
+	}
+	podSpec.InitContainers = []corev1.Container{
+		{
+			Name:  "test-init-container-0",
+			Image: "ubuntu",
+			Env: []corev1.EnvVar{{
+				Name:  "TEST_INIT_0",
+				Value: "value",
+			}},
+		},
+		{
+			Name:  "dash0-instrumentation",
+			Image: "ubuntu",
+		},
+		{
+			Name:  "test-init-container-2",
+			Image: "ubuntu",
+			Env: []corev1.EnvVar{
+				{
+					Name:  "TEST_INIT_0",
+					Value: "value",
+				},
+				{
+					Name:  "TEST_INIT_1",
+					Value: "value",
+				},
+				{
+					Name:  "TEST_INIT_2",
+					Value: "value",
+				},
+			},
+		},
+	}
+	podSpec.Containers = append(
+		deployment.Spec.Template.Spec.Containers,
+		corev1.Container{
+			Name:  "test-container-1",
+			Image: "ubuntu",
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "dash0-instrumentation",
+					MountPath: "/will/be/overwritten",
+				},
+				{
+					Name:      "test-volume-0",
+					MountPath: "/test-1",
+				},
+			},
+			Env: []corev1.EnvVar{{
+				Name:  "TEST0",
+				Value: "value",
+			}},
+		},
+		corev1.Container{
+			Name:  "test-container-2",
+			Image: "ubuntu",
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "test-volume-0",
+					MountPath: "/test-0",
+				},
+				{
+					Name:      "dash0-instrumentation",
+					MountPath: "/will/be/overwritten",
+				},
+				{
+					Name:      "test-volume-2",
+					MountPath: "/test-2",
+				},
+			},
+			Env: []corev1.EnvVar{
+				{
+					Name:  "TEST0",
+					Value: "value",
+				},
+				{
+					Name:      "TEST1",
+					ValueFrom: &corev1.EnvVarSource{FieldRef: &corev1.ObjectFieldSelector{FieldPath: "metadata.namespace"}},
+				},
+			},
+		})
+
 	return deployment
 }
 
