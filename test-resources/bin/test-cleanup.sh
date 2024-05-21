ac054248238c34290d458d32f54a03ca6160fa74
Bastian Krol <bastian.krol@dash0.com>
2024-05-21 21:22:37 +0200

    refactor: reorganize test roundtrip scripts

--- test-resources/bin/test-cleanup.sh
+++ test-resources/bin/test-cleanup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd "$(dirname ${BASH_SOURCE})"/..
+cd "$(dirname ${BASH_SOURCE})"/../..
 
 kubectl delete -k config/samples || true
 make uninstall || true
 make undeploy || true
 test-resources/node.js/express/undeploy.sh
+test-resources/collector/undeploy.sh
