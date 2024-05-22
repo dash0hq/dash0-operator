75212117603022734d27309756aa68e2e4465bf4
Bastian Krol <bastian.krol@dash0.com>
2024-05-22 09:02:07 +0200

    chore: improve scripts for semi-manual testing

--- test-resources/bin/test-cleanup.sh
+++ test-resources/bin/test-cleanup.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2024 Dash0 Inc.
+# SPDX-License-Identifier: Apache-2.0
+
 set -euo pipefail
+
 
 cd "$(dirname ${BASH_SOURCE})"/../..
 
-kubectl delete -k config/samples || true
+target_namespace=${1:-default}
+
+kubectl delete -n ${target_namespace} -k config/samples || true
 make uninstall || true
 make undeploy || true
-test-resources/node.js/express/undeploy.sh
-test-resources/collector/undeploy.sh
+test-resources/node.js/express/undeploy.sh ${target_namespace}
+test-resources/collector/undeploy.sh ${target_namespace}
--- test-resources/cert-manager/deploy.sh
+++ test-resources/cert-manager/deploy.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2024 Dash0 Inc.
+# SPDX-License-Identifier: Apache-2.0
+
 set -euo pipefail
 
 cd "$(dirname ${BASH_SOURCE})"
 
-
-echo undeploying
+echo "removing any left-overs from previous cert-manager installations (if any)"
 ./undeploy.sh
 
-echo deploying cert-manager
+echo "deploying cert-manager and waiting for it to become ready, this might take up to five minutes"
 helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
   --version v1.14.5 \
   --set installCRDs=true \
-  --timeout 10s
+  --timeout 5m
