20833f378c515aa614188e256afdff8dbb964dc5
Bastian Krol <bastian.krol@dash0.com>
2024-06-03 09:22:52 +0200

    chore: add k8sattributes processor to collector

--- test-resources/bin/render-templates.sh
+++ test-resources/bin/render-templates.sh
@@ -15,13 +15,18 @@ done
 
 if [[ ${1:-} == manual-testing ]]; then
   # Generate collector values file for manual testing:
-  if [[ -n ${DASH0_AUTHORIZATION_TOKEN:-} ]] && [[ -n ${OTEL_EXPORTER_OTLP_ENDPOINT:-} ]] then
-      echo "Using DASH0_AUTHORIZATION_TOKEN and OTEL_EXPORTER_OTLP_ENDPOINT to render the template test-resources/collector/manual.values.yaml.template. The rendered file test-resources/collector/manual.values.yaml can be used for manual testing and actually reporting data to Dash0."
+  if [[ -n ${DASH0_AUTHORIZATION_TOKEN:-} ]] && [[ -n ${DASH0_OTEL_EXPORTER_OTLP_ENDPOINT:-} ]] then
+      echo "Using DASH0_AUTHORIZATION_TOKEN and DASH0_OTEL_EXPORTER_OTLP_ENDPOINT to render the template test-resources/collector/manual.values.yaml.template. The rendered file test-resources/collector/manual.values.yaml can be used for manual testing and actually reporting data to Dash0."
       cat test-resources/collector/manual.values.yaml.template | envsubst > test-resources/collector/manual.values.yaml
   elif [[ -f test-resources/collector/manual.values.yaml ]]; then
-    echo "The file test-resources/collector/manual.values.yaml already exists, the file will be reused. Set DASH0_AUTHORIZATION_TOKEN and OTEL_EXPORTER_OTLP_ENDPOINT to generate a new file."
+    if [[ test-resources/collector/manual.values.yaml.template -nt test-resources/collector/manual.values.yaml ]]; then
+      echo "The file test-resources/collector/manual.values.yaml exists, but the template file test-resources/collector/manual.values.yaml.template is newer, indicating it has been changed after the last time the template has been rendered. Refusing to run with an outdated manual.values.yaml file. Also, DASH0_AUTHORIZATION_TOKEN or DASH0_OTEL_EXPORTER_OTLP_ENDPOINT are not set (or both). Both environment variables are required to regenerate manual.values.yaml for the OpenTelemetry collector. Please set both environment variables and try again."
+      exit 1
+    else
+      echo "The file test-resources/collector/manual.values.yaml already exists and seems up to date, the file will be reused. Set DASH0_AUTHORIZATION_TOKEN and DASH0_OTEL_EXPORTER_OTLP_ENDPOINT to generate a new file."
+    fi
   else
-    echo "The file test-resources/collector/manual.values.yaml does not exist and either DASH0_AUTHORIZATION_TOKEN or OTEL_EXPORTER_OTLP_ENDPOINT are not set (or both). Both environment variables are required when running this script for the first time, to generate a valid manual.values.yaml file for the OpenTelemetry collector. They can be omitted on subsequent runs, unless test-resources/collector/manual.values.yaml is deleted and needs to be regenerated."
+    echo "The file test-resources/collector/manual.values.yaml does not exist and either DASH0_AUTHORIZATION_TOKEN or DASH0_OTEL_EXPORTER_OTLP_ENDPOINT are not set (or both). Both environment variables are required when running this script for the first time, to generate a valid manual.values.yaml file for the OpenTelemetry collector. They can be omitted on subsequent runs, unless test-resources/collector/manual.values.yaml is deleted and needs to be regenerated."
     exit 1
   fi
 else
