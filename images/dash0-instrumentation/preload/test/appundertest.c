7e83ac731c612353b256390c5b1f4ae37a1f4d77
Bastian Krol <bastian.krol@dash0.com>
2024-06-21 18:01:50 +0200

    test(preload): improve smoke test script

--- images/dash0-instrumentation/preload/test/appundertest.c
+++ images/dash0-instrumentation/preload/test/appundertest.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void echo_env_var(const char* name) {
   fputs(name, stdout);
@@ -14,9 +15,8 @@ void echo_env_var(const char* name) {
   if (value != NULL) {
     fputs(value, stdout);
   } else {
-    fputs("not set", stdout);
+    fputs("NULL", stdout);
   }
-  fputs("; ", stdout);
 }
 
 void echo_env_var_secure(const char* name) {
@@ -27,16 +27,32 @@ void echo_env_var_secure(const char* name) {
   if (value != NULL) {
     fputs(value, stdout);
   } else {
-    fputs("not set", stdout);
+    fputs("NULL", stdout);
   }
-  fputs("; ", stdout);
 }
 
-int main(void) {
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    fputs("not enough arguments, the name of the test case needs to be specifed", stdout);
+    fputs("\n", stdout);
+    exit(1);
+  }
+  char* test_case = argv[1];
+  if (strcmp(test_case, "non-existing") == 0) {
+    echo_env_var("DOES_NOT_EXIST");
+  } else if (strcmp(test_case, "term") == 0) {
     echo_env_var("TERM");
+  } else if (strcmp(test_case, "node_options") == 0) {
     echo_env_var("NODE_OPTIONS");
-    echo_env_var("TERM");
+  } else if (strcmp(test_case, "term-gnu-secure") == 0) {
+    echo_env_var_secure("TERM");
+  } else if (strcmp(test_case, "node_options-gnu-secure") == 0) {
     echo_env_var_secure("NODE_OPTIONS");
+  } else {
+    fputs("unknown test case:", stdout);
+    fputs(test_case, stdout);
+    fputs("\n", stdout);
+    exit(1);
+  }
 }
-
 
