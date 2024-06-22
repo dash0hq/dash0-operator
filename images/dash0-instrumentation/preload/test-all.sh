dcf09d26998354a112126d02dd9a607b5f38b61c
Bastian Krol <bastian.krol@dash0.com>
2024-06-22 10:53:17 +0200

    test(preload): test against glibc and musl
    Also, convert all shell scripts to POSIX-compliant sh scripts and apply
    most shellcheck suggestions.

--- images/dash0-instrumentation/preload/test-all.sh
+++ images/dash0-instrumentation/preload/test-all.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # SPDX-FileCopyrightText: Copyright 2024 Dash0 Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-set -euo pipefail
+set -eu
 
-cd "$(dirname ${BASH_SOURCE})"
+cd "$(dirname "$0")"
 
-ARCH=arm64 ./build-and-test.sh
-ARCH=x86_64 ./build-and-test.sh
+ARCH=arm64 LIBC=glibc ./build-and-test.sh
+ARCH=x86_64 LIBC=glibc ./build-and-test.sh
+ARCH=arm64 LIBC=musl ./build-and-test.sh
+ARCH=x86_64 LIBC=musl ./build-and-test.sh
 
