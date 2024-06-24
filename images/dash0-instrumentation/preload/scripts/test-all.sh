7190db61f0af7b12db500a85be42469ad10abb64
Bastian Krol <bastian.krol@dash0.com>
2024-06-24 12:37:08 +0200

    test(preload): run tests for both CPU architectures
    [skip ci]

--- images/dash0-instrumentation/preload/scripts/test-all.sh
+++ images/dash0-instrumentation/preload/scripts/test-all.sh
@@ -5,12 +5,44 @@
 
 set -eu
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 cd "$(dirname "$0")"/..
 
-scripts/build-in-container.sh
+ARCH=arm64 scripts/build-in-container.sh
+ARCH=x86_64 scripts/build-in-container.sh
 
-ARCH=arm64 LIBC=glibc scripts/run-tests-in-container.sh
-ARCH=x86_64 LIBC=glibc scripts/run-tests-in-container.sh
-ARCH=arm64 LIBC=musl scripts/run-tests-in-container.sh
-ARCH=x86_64 LIBC=musl scripts/run-tests-in-container.sh
+exit_code=0
+summary=""
+
+run_tests_for_architecture_and_libc_flavor() {
+  arch=$1
+  libc=$2
+  set +e
+  ARCH=$arch LIBC=$libc scripts/run-tests-in-container.sh
+  test_exit_code=$?
+  set -e
+  echo
+  echo ---------------------------------------
+  if [ $test_exit_code != 0 ]; then
+    printf "${RED}tests for %s/%s failed (see above for details)${NC}\n" "$arch" "$libc"
+    exit_code=1
+    summary="$summary\n$arch/$libc:\tfailed"
+  else
+    printf "${GREEN}tests for %s/%s were successful${NC}\n" "$arch" "$libc"
+    summary="$summary\n$arch/$libc:\tok"
+  fi
+  echo ---------------------------------------
+  echo
+}
+
+run_tests_for_architecture_and_libc_flavor arm64 glibc
+run_tests_for_architecture_and_libc_flavor x86_64 glibc
+run_tests_for_architecture_and_libc_flavor arm64 musl
+run_tests_for_architecture_and_libc_flavor x86_64 musl
+
+echo "$summary"
+exit $exit_code
 
