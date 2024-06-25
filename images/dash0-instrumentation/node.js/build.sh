9c651238cd6a114f803cdac5f539f030216e80e3
Bastian Krol <bastian.krol@dash0.com>
2024-06-25 11:42:45 +0200

    chore(instrumentation image): use Node distribution from npm by default
    [skip ci]

--- images/dash0-instrumentation/node.js/build.sh
+++ images/dash0-instrumentation/node.js/build.sh
@@ -3,17 +3,36 @@ set -euo pipefail
 
 cd "$(dirname ${BASH_SOURCE})"
 
-rm -f dash0hq-opentelemetry-*.tgz
-pushd ../../../../opentelemetry-js-distribution > /dev/null
-npm pack
-popd > /dev/null
-cp ../../../../opentelemetry-js-distribution/dash0hq-opentelemetry-*.tgz .
+if [[ "${USE_LOCAL_SOURCES_FOR_NODEJS_DISTRIBUTION:-}" = true ]]; then
+  echo "Node.js: using the local sources for @dash0hq/opentelemetry"
+  rm -f dash0hq-opentelemetry-*.tgz
+  pushd ../../../../opentelemetry-js-distribution > /dev/null
+  npm pack
+  popd > /dev/null
+  cp ../../../../opentelemetry-js-distribution/dash0hq-opentelemetry-*.tgz .
 
-NPM_CONFIG_UPDATE_NOTIFIER=false \
+  NPM_CONFIG_UPDATE_NOTIFIER=false \
+    npm install \
+    --package-lock-only \
+    --ignore-scripts \
+    --omit=dev \
+    --no-audit \
+    --no-fund=true \
+    dash0hq-opentelemetry-*.tgz
+
+else
+  nodejs_distribution_version="1.0.1"
+  fully_qualified_package_name="@dash0hq/opentelemetry@${nodejs_distribution_version}"
+  rm -f dash0hq-opentelemetry-*.tgz
+  echo "Node.js: using @dash0hq/opentelemetry@${nodejs_distribution_version}"
+
+  NPM_CONFIG_UPDATE_NOTIFIER=false \
   npm install \
+  --save-exact \
   --package-lock-only \
   --ignore-scripts \
   --omit=dev \
   --no-audit \
   --no-fund=true \
-  dash0hq-opentelemetry-*.tgz
+  "$fully_qualified_package_name"
+fi
