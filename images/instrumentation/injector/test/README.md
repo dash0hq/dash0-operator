1504336dee33a72cb03a8683e8edfb15e0e4c455
Bastian Krol <bastian.krol@dash0.com>
2024-10-23 11:25:29 +0200

    test(instrumentation): use multi-platform instrumentation image in tests

--- images/instrumentation/injector/test/README.md
+++ images/instrumentation/injector/test/README.md
@@ -1,2 +1,4 @@
 This directory contains isolated tests for the injector code.
 The difference to images/instrumentation/test is that the latter tests the whole instrumentation image.
+Also, the tests in this folder do not use multi-platform images, an injector binary is build (in a container) per CPU
+architecture, and then used for testing.
--- images/instrumentation/injector/test/scripts/test-all.sh
+++ images/instrumentation/injector/test/scripts/test-all.sh
@@ -9,17 +9,41 @@ set -eu
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+dockerfile_injector_build=injector/test/docker/Dockerfile-build
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/../../..
 
 # remove all outdated injector binaries
 rm -rf injector/test/bin/*
 
+exit_code=0
+summary=""
+run_tests_for_architecture_and_libc_flavor() {
+  arch=$1
+  libc=$2
+  set +e
+  ARCH=$arch LIBC=$libc injector/test/scripts/run-tests-for-container.sh
+  test_exit_code=$?
+  set -e
+  echo
+  echo ----------------------------------------
+  if [ $test_exit_code != 0 ]; then
+    printf "${RED}tests for %s/%s failed (see above for details)${NC}\n" "$arch" "$libc"
+    exit_code=1
+    summary="$summary\n$arch/$libc:\t${RED}failed${NC}"
+  else
+    printf "${GREEN}tests for %s/%s were successful${NC}\n" "$arch" "$libc"
+    summary="$summary\n$arch/$libc:\t${GREEN}ok${NC}"
+  fi
+  echo ----------------------------------------
+  echo
+}
+
 # Create a Dockerfile for building the injector in a container by re-using an excerpt from the ../Dockerfile. This makes
 # sure we keep the way we build the injector binary here for this test suite and for actual production usage in the
 # instrumentation image in sync.
 copy_lines=false
-rm -f injector/test/docker/Dockerfile-build
+rm -f "$dockerfile_injector_build"
 while IFS= read -r line; do
   if [[ "$line" =~ ^[[:space:]]*#.* ]]; then
     # skip comments
@@ -32,41 +56,18 @@ while IFS= read -r line; do
     copy_lines=true
   fi
   if [[ $copy_lines == true ]]; then
-    echo "$line" >> injector/test/docker/Dockerfile-build
+    echo "$line" >> "$dockerfile_injector_build"
   fi
 done < Dockerfile
 
-if [[ ! -e injector/test/docker/Dockerfile-build ]]; then
-  printf "\nError: The file injector/test/docker/Dockerfile-build has not been generated, stopping."
+if [[ ! -e "$dockerfile_injector_build" ]]; then
+  printf "\nError: The file $dockerfile_injector_build has not been generated, stopping."
   exit 1
 fi
 
 # build injector binary for both architectures
 ARCH=arm64 injector/test/scripts/build-in-container.sh
 ARCH=x86_64 injector/test/scripts/build-in-container.sh
-
-exit_code=0
-summary=""
-run_tests_for_architecture_and_libc_flavor() {
-  arch=$1
-  libc=$2
-  set +e
-  ARCH=$arch LIBC=$libc injector/test/scripts/run-tests-for-container.sh
-  test_exit_code=$?
-  set -e
-  echo
-  echo ---------------------------------------
-  if [ $test_exit_code != 0 ]; then
-    printf "${RED}tests for %s/%s failed (see above for details)${NC}\n" "$arch" "$libc"
-    exit_code=1
-    summary="$summary\n$arch/$libc:\tfailed"
-  else
-    printf "${GREEN}tests for %s/%s were successful${NC}\n" "$arch" "$libc"
-    summary="$summary\n$arch/$libc:\tok"
-  fi
-  echo ---------------------------------------
-  echo
-}
 
 run_tests_for_architecture_and_libc_flavor arm64 glibc
 run_tests_for_architecture_and_libc_flavor x86_64 glibc
