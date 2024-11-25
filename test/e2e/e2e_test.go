bcf869c672b868fe6da518340692248d6eddefe3
Bastian Krol <bastian.krol@dash0.com>
2024-11-25 16:37:34 +0100

    test(e2e): improve output for matching spans

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -966,10 +965,6 @@ var _ = Describe("Dash0 Operator", Ordered, func() {
 	})
 })
 
-type workloadConfig interface {
-	GetWorkloadType() string
-}
-
 func (wt workloadType) GetWorkloadType() string {
 	return wt.workloadTypeString
 }
@@ -981,43 +976,6 @@ type removalTestNamespaceConfig struct {
 
 func (c removalTestNamespaceConfig) GetWorkloadType() string {
 	return c.workloadType.workloadTypeString
-}
-
-func runInParallelForAllWorkloadTypes[C workloadConfig](
-	workloadConfigs []C,
-	testStep func(C),
-) {
-	var passedMutex sync.Mutex
-	passed := make(map[string]bool)
-	var wg sync.WaitGroup
-	for _, config := range workloadConfigs {
-		workloadTypeString := config.GetWorkloadType()
-		passed[workloadTypeString] = false
-		wg.Add(1)
-		go func(cfg C) {
-			defer GinkgoRecover()
-			defer wg.Done()
-			e2ePrint("(before test step: %s)\n", workloadTypeString)
-			testStep(cfg)
-			e2ePrint("(after test step: %s)\n", workloadTypeString)
-			passedMutex.Lock()
-			passed[workloadTypeString] = true
-			passedMutex.Unlock()
-		}(config)
-	}
-	wg.Wait()
-
-	// Fail early if one of the workloads has not passed the test step. Because of runInParallelForAllWorkloadTypes and
-	// the business with the (required) "defer GinkgoRecover()", Ginkgo needs a little help with that. Without this
-	// additional check, a failure occurring in testStep might not make the test fail immediately, but is only reported
-	// after the whole test has finished. This might lead to some slightly weird and hard-to-understand behavior,
-	// because it looks like the has passed testStep, and then the whole test fails with something that should have been
-	// reported much earlier.
-	for _, config := range workloadConfigs {
-		if !passed[config.GetWorkloadType()] {
-			Fail(fmt.Sprintf("workload %s has not passed a test step executed in parallel", config.GetWorkloadType()))
-		}
-	}
 }
 
 func cleanupAll() {
