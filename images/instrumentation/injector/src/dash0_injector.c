48a26c4d6228ac0abe0234fa0bf63c042b4da965
Michele Mancioppi <michele.mancioppi@dash0.com>
2024-12-02 21:19:24 +0100

    feat(injector): set k8s.pod.uid and k8s.container.name out of the box

--- images/instrumentation/injector/src/dash0_injector.c
+++ images/instrumentation/injector/src/dash0_injector.c
@@ -9,6 +9,9 @@
 
 #define NODE_OPTIONS_ENV_VAR_NAME "NODE_OPTIONS"
 #define NODE_OPTIONS_DASH0_REQUIRE "--require /__dash0__/instrumentation/node.js/node_modules/@dash0hq/opentelemetry"
+#define OTEL_RESOURCE_ATTRIBUTES_ENV_VAR_NAME "OTEL_RESOURCE_ATTRIBUTES"
+#define DASH0_POD_UID_ENV_VAR_NAME "DASH0_POD_UID"
+#define DASH0_POD_CONTAINER_NAME_VAR_NAME "DASH0_CONTAINER_NAME"
 
 extern char **__environ;
 
@@ -85,28 +88,70 @@ char val2[1012];
 char *getenv(const char *name)
 {
     char *origValue = __getenv(name);
+	char *podUid = __getenv(DASH0_POD_UID_ENV_VAR_NAME);
+	char *containerName = __getenv(DASH0_POD_CONTAINER_NAME_VAR_NAME);
     int l = __strlen(name);
 
+	char *otelResourceAttributesVarName = OTEL_RESOURCE_ATTRIBUTES_ENV_VAR_NAME;
     char *nodeOptionsVarName = NODE_OPTIONS_ENV_VAR_NAME;
-    if (__strcmp(name, nodeOptionsVarName) == 0)
-    {
+	if (__strcmp(name, otelResourceAttributesVarName) == 0)
+	{
+		// If we have not calculated this env var yet
         if (__strlen(val1) == 0)
         {
-            // Prepend our --require as the first item to the NODE_OPTIONS string.
-            char *nodeOptionsDash0Require = NODE_OPTIONS_DASH0_REQUIRE;
-            __strcat(val1, nodeOptionsDash0Require);
+			int catCount = 0;
 
-            if (origValue != NULL)
+			if (podUid != NULL && __strlen(podUid) > 0)
+			{
+				__strcat(val1, "k8s.pod.uid=");
+                __strcat(val1, podUid);
+				catCount += 1;
+			}
+
+			if (containerName != NULL && __strlen(containerName) > 0)
+			{
+				if (catCount > 0)
+				{
+					__strcat(val1, ",");
+				}
+
+				__strcat(val1, "k8s.container.name=");
+                __strcat(val1, containerName);
+				catCount += 1;
+			}
+
+            if (origValue != NULL && __strlen(origValue) > 0)
             {
-                // If NODE_OPTIONS were present, append the existing NODE_OPTIONS after our --require.
-                __strcat(val1, " ");
+				if (catCount > 0)
+				{
+					__strcat(val1, ",");
+				}
+
                 __strcat(val1, origValue);
             }
         }
 
         return val1;
+	}
+    else if (__strcmp(name, nodeOptionsVarName) == 0)
+    {
+		// If we have not calculated this env var yet
+        if (__strlen(val2) == 0)
+        {
+            // Prepend our --require as the first item to the NODE_OPTIONS string.
+            char *nodeOptionsDash0Require = NODE_OPTIONS_DASH0_REQUIRE;
+            __strcat(val2, nodeOptionsDash0Require);
+
+            if (origValue != NULL && __strlen(origValue) > 0)
+            {
+                // If NODE_OPTIONS were present, append the existing NODE_OPTIONS after our --require.
+                __strcat(val2, " ");
+                __strcat(val2, origValue);
+            }
+        }
+
+        return val2;
     }
 
     return origValue;
 }
-
