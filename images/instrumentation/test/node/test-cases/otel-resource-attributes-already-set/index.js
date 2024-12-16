60c9afa1ed57e68e323002d47566bc3eba428c37
Bastian Krol <bastian.krol@dash0.com>
2024-12-16 16:59:46 +0100

    test(instrumentation): more test cases for OTEL_RESOURCE_ATTRIBUTES

---
+++ images/instrumentation/test/node/test-cases/otel-resource-attributes-already-set/index.js
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: Copyright 2024 Dash0 Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+const process = require('node:process');
+
+if (process.env["OTEL_RESOURCE_ATTRIBUTES"] !== "k8s.namespace.name=namespace,k8s.pod.name=pod_name,k8s.pod.uid=pod_uid,k8s.container.name=container_name,key1=value1,key2=value2") {
+  console.error(`Unexpected value for OTEL_RESOURCE_ATTRIBUTES: ${process.env["OTEL_RESOURCE_ATTRIBUTES"]}`);
+  process.exit(1);
+}
