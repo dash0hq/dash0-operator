100966d448ced6c5a47609a455fda6121023bcfe
Michele Mancioppi <michele.mancioppi@dash0.com>
2025-01-05 18:21:14 +0100

    chore: reuse maven cache when building Java test cases

--- images/instrumentation/test/jvm/test-cases/spring-micrometer-otel/src/test/java/com/dash0/injector/spring_micrometer_otel/TestApplicationTests.java
+++ images/instrumentation/test/jvm/test-cases/spring-micrometer-otel/src/test/java/com/dash0/injector/spring_micrometer_otel/TestApplicationTests.java
@@ -2,10 +2,8 @@ package com.dash0.injector.spring_micrometer_otel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {"spring.sleuth.enabled=false"})
 class TestApplicationTests {
 
 	@Test
--- images/instrumentation/test/test-all.sh
+++ images/instrumentation/test/test-all.sh
@@ -72,7 +72,7 @@ run_tests_for_runtime() {
   for t in "${script_dir}"/"${runtime}"/test-cases/*/ ; do
     test=$(basename "$(realpath "${t}")")
 
-    if [ "${runtime}" = "jvm" ] && mvn_package_output=$("${script_dir}/${runtime}/test-cases/${test}/mvnw" package -f "${script_dir}/${runtime}/test-cases/${test}" 2>&1) && docker_run_output=$(docker run \
+    if [ "${runtime}" = "jvm" ] && docker_run_output=$(docker run \
       --env-file="${script_dir}/${runtime}/test-cases/${test}/.env" \
       "${image_name_test}" \
       java -jar "/test-cases/${test}/target/app.jar" \
@@ -89,7 +89,6 @@ run_tests_for_runtime() {
     else
       printf "${RED}test case \"${test}\": FAIL\n"
       printf "test output:${NC}\n"
-      echo "$mvn_package_output"
       echo "$docker_run_output"
       exit_code=1
       summary="$summary\n${runtime}/${base_image}\t- ${test}:\tfailed"
