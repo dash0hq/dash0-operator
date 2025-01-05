43c99850eed47ac86c92f113afd782c6e410558f
Michele Mancioppi <michele.mancioppi@dash0.com>
2025-01-05 16:10:57 +0100

    fix: avoid test deadlock

--- images/instrumentation/test/jvm/test-cases/spring-micrometer-otel/src/main/java/com/dash0/injector/spring_micrometer_otel/TestApplication.java
+++ images/instrumentation/test/jvm/test-cases/spring-micrometer-otel/src/main/java/com/dash0/injector/spring_micrometer_otel/TestApplication.java
@@ -1,23 +1,16 @@
 package com.dash0.injector.spring_micrometer_otel;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TestApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TestApplication.class);
-
-	@GetMapping("/")
-	public String index() {
-		LOGGER.info("Greetings from Spring Boot!");
-		return "Greetings from Spring Boot!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
