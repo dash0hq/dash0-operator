2072cb0d874ba6b326e6200c682d1a0ec922a201
Bastian Krol <bastian.krol@dash0.com>
2025-02-17 15:45:06 +0100

    chore(instrumentation): remove separate DASH0_INJECTOR_FOLDER_SOURCE

--- images/instrumentation/copy-instrumentation.sh
+++ images/instrumentation/copy-instrumentation.sh
@@ -11,15 +11,8 @@ fi
 
 cd -P -- "$(dirname -- "$0")"
 
-if [ -z "${DASH0_INJECTOR_FOLDER_SOURCE:-}" ]; then
-  DASH0_INJECTOR_FOLDER_SOURCE=/dash0-init-container
-fi
-if [ ! -d "${DASH0_INJECTOR_FOLDER_SOURCE}" ]; then
-  >&2 echo "[Dash0] injector source directory ${DASH0_INJECTOR_FOLDER_SOURCE} does not exist."
-  exit 1
-fi
 if [ -z "${DASH0_INSTRUMENTATION_FOLDER_SOURCE:-}" ]; then
-  DASH0_INSTRUMENTATION_FOLDER_SOURCE=/dash0-init-container/instrumentation
+  DASH0_INSTRUMENTATION_FOLDER_SOURCE=/dash0-init-container
 fi
 if [ ! -d "${DASH0_INSTRUMENTATION_FOLDER_SOURCE}" ]; then
   >&2 echo "[Dash0] Instrumentation source directory ${DASH0_INSTRUMENTATION_FOLDER_SOURCE} does not exist."
@@ -31,11 +24,8 @@ if [ -z "${DASH0_INSTRUMENTATION_FOLDER_DESTINATION:-}" ]; then
 fi
 
 # We deliberately do not create the base directory for $DASH0_INSTRUMENTATION_FOLDER_DESTINATION via mkdir, it needs
-# be an existing mount point provided externally.
-cp -R "${DASH0_INSTRUMENTATION_FOLDER_SOURCE}"/ "${DASH0_INSTRUMENTATION_FOLDER_DESTINATION}"
-
-# Copy the injector from the init container to the monitored container.
-cp "${DASH0_INJECTOR_FOLDER_SOURCE}"/*.so "${DASH0_INSTRUMENTATION_FOLDER_DESTINATION}"/
+# to be an existing mount point provided externally.
+cp -R "${DASH0_INSTRUMENTATION_FOLDER_SOURCE}"/* "${DASH0_INSTRUMENTATION_FOLDER_DESTINATION}"
 
 if [ -n "${DASH0_DEBUG:-}" ]; then
   find "${DASH0_INSTRUMENTATION_FOLDER_DESTINATION}"
