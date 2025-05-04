67d03b42ed6b2ad87d58a33536dddbec471f77ac
Bastian Krol <bastian.krol@dash0.com>
2025-05-05 00:33:55 +0200

    test(instrumentation): add test for JVM -Dotel.resource.attributes

--- images/instrumentation/test/jvm/test-cases/otel-resource-attributes-env-var-already-set/Main.java
+++ images/instrumentation/test/jvm/test-cases/otel-resource-attributes-env-var-already-set/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String value = System.getenv("OTEL_RESOURCE_ATTRIBUTES");
-        if (!value.contains("key1=value1,key2=value2")) {
+        if (!"key1=value1,key2=value2".equals(value)) {
             throw new RuntimeException(String.format("Unexpected value for the 'OTEL_RESOURCE_ATTRIBUTES' env var: %s", value));
         }
     }
