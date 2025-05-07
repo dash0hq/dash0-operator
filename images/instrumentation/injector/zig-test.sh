8679d77edcc947f590c13225f995f9eecaa9d121
Bastian Krol <bastian.krol@dash0.com>
2025-05-07 17:06:38 +0200

    test(instrumentation) remove test containers&images after running tests

--- images/instrumentation/injector/zig-test.sh
+++ images/instrumentation/injector/zig-test.sh
@@ -5,13 +5,17 @@
 
 # A small helper script intended to be used with entr, like so:
 # cd images/instrumentation/injector
-# fd | entr ./test.sh
+# fd | entr ./zig-test.sh
 # ...to get fast feedback on test errors when working on the Zig code.
 
 set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-zig build test
-echo "$(date) tests successful"
-
+if zig build test; then
+  echo "$(date) tests successful"
+  echo
+else
+  echo "$(date) tests failed"
+  echo
+fi
