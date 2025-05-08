c1e5d5026562f99211a9cfba63b1027da5f80a07
Bastian Krol <bastian.krol@dash0.com>
2025-05-08 18:44:47 +0200

    chore(injector): split into smaller zig files

---
+++ images/instrumentation/injector/src/dotnet.zig
@@ -0,0 +1,227 @@
+// SPDX-FileCopyrightText: Copyright 2025 Dash0 Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+const builtin = @import("builtin");
+const std = @import("std");
+
+const alloc = @import("./allocator.zig");
+const print = @import("./print.zig");
+const types = @import("./types.zig");
+
+pub const DotNetValues = struct {
+    coreclr_enable_profiling: types.NullTerminatedString,
+    coreclr_profiler: types.NullTerminatedString,
+    coreclr_profiler_path: types.NullTerminatedString,
+    additional_deps: types.NullTerminatedString,
+    shared_store: types.NullTerminatedString,
+    startup_hooks: types.NullTerminatedString,
+    otel_auto_home: types.NullTerminatedString,
+};
+
+const LibCFlavor = enum { UNKNOWN, GNU_LIBC, MULSC };
+
+const dotnet_path_prefix = "/__dash0__/instrumentation/dotnet";
+var cached_dotnet_values: ?DotNetValues = null;
+var cached_libc_flavor: ?LibCFlavor = null;
+
+pub fn getDotNetValues() ?DotNetValues {
+    if (cached_dotnet_values) |val| {
+        return val;
+    }
+
+    if (cached_libc_flavor == null) {
+        cached_libc_flavor = getLibCFlavor();
+    }
+
+    if (cached_libc_flavor == LibCFlavor.UNKNOWN) {
+        print.printError("Cannot determine LibC flavor", .{});
+        return null;
+    }
+
+    if (cached_libc_flavor) |flavor| {
+        return determineDotNetValues(flavor) catch |err| {
+            print.printError("Cannot determine .NET environment variables: {}", .{err});
+            return null;
+        };
+    }
+
+    unreachable;
+}
+
+fn determineDotNetValues(flavor: LibCFlavor) !DotNetValues {
+    const libc_flavor_prefix = if (flavor == LibCFlavor.GNU_LIBC) "glibc" else "musl";
+    const platform =
+        if (flavor == LibCFlavor.GNU_LIBC)
+            (if (builtin.cpu.arch == .aarch64) "linux-arm64" else "linux-x64")
+        else
+            (if (builtin.cpu.arch == .aarch64) "linux-musl-arm64" else "linux-musl-x64");
+
+    const coreclr_profiler_path = try std.fmt.allocPrintZ(alloc.allocator, "{s}/{s}/{s}/OpenTelemetry.AutoInstrumentation.Native.so", .{
+        dotnet_path_prefix, libc_flavor_prefix, platform,
+    });
+
+    const additional_deps = try std.fmt.allocPrintZ(alloc.allocator, "{s}/{s}/AdditionalDeps", .{
+        dotnet_path_prefix, libc_flavor_prefix,
+    });
+
+    const otel_auto_home = try std.fmt.allocPrintZ(alloc.allocator, "{s}/{s}", .{ dotnet_path_prefix, libc_flavor_prefix });
+
+    const shared_store = try std.fmt.allocPrintZ(alloc.allocator, "{s}/{s}/store", .{
+        dotnet_path_prefix, libc_flavor_prefix,
+    });
+
+    const startup_hooks = try std.fmt.allocPrintZ(alloc.allocator, "{s}/{s}/net/OpenTelemetry.AutoInstrumentation.StartupHook.dll", .{
+        dotnet_path_prefix, libc_flavor_prefix,
+    });
+
+    return .{
+        .coreclr_enable_profiling = "1",
+        .coreclr_profiler = "{918728DD-259F-4A6A-AC2B-B85E1B658318}",
+        .coreclr_profiler_path = coreclr_profiler_path,
+        .additional_deps = additional_deps,
+        .otel_auto_home = otel_auto_home,
+        .shared_store = shared_store,
+        .startup_hooks = startup_hooks,
+    };
+}
+
+fn getLibCFlavor() LibCFlavor {
+    return doGetLibCFlavor() catch |err| {
+        print.printError("Cannot determine LibC flavor from ELF metadata of '/proc/self/exe': {}", .{err});
+        return LibCFlavor.UNKNOWN;
+    };
+}
+
+fn doGetLibCFlavor() !LibCFlavor {
+    const self_exe_file = std.fs.openFileAbsolute("/proc/self/exe", .{ .mode = .read_only }) catch |err| {
+        print.printError("Cannot open '/proc/self/exe': {}", .{err});
+        return LibCFlavor.UNKNOWN;
+    };
+    defer self_exe_file.close();
+
+    const elf_header = std.elf.Header.read(self_exe_file) catch |err| {
+        print.printError("Cannot read ELF header from '/proc/self/exe': {}", .{err});
+        return LibCFlavor.UNKNOWN;
+    };
+
+    if (!elf_header.is_64) {
+        print.printError("ELF header from '/proc/self/exe' seems not to be the one of a 64-bit binary", .{});
+        return error.ElfNot64Bit;
+    }
+
+    var sections_header_iterator = elf_header.section_header_iterator(self_exe_file);
+
+    var dynamic_symbols_table_offset: u64 = 0;
+    var dynamic_symbols_table_size: u64 = 0;
+
+    while (try sections_header_iterator.next()) |section_header| {
+        switch (section_header.sh_type) {
+            std.elf.SHT_DYNAMIC => {
+                dynamic_symbols_table_offset = section_header.sh_offset;
+                dynamic_symbols_table_size = section_header.sh_size;
+            },
+            else => {
+                // Ignore this section
+            },
+        }
+    }
+
+    if (dynamic_symbols_table_offset == 0) {
+        print.printError("No dynamic section found in ELF metadata from '/proc/self/exe'", .{});
+        return error.ElfDynamicSymbolTableNotFound;
+    }
+
+    // Look for DT_NEEDED entries in the Dynamic table, they state which libraries were
+    // used at compilation step. Examples:
+    //
+    // Java + GNU LibC
+    //
+    // $ readelf -Wd /usr/bin/java
+    // Dynamic section at offset 0xfd28 contains 30 entries:
+    //   Tag        Type                         Name/Value
+    //  0x0000000000000001 (NEEDED)             Shared library: [libz.so.1]
+    //  0x0000000000000001 (NEEDED)             Shared library: [libjli.so]
+    //  0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
+    //
+    // Java + musl
+    //
+    // $ readelf -Wd /usr/bin/java
+    // Dynamic section at offset 0xfd18 contains 33 entries:
+    //   Tag        Type                         Name/Value
+    //  0x0000000000000001 (NEEDED)             Shared library: [libjli.so]
+    //  0x0000000000000001 (NEEDED)             Shared library: [libc.musl-aarch64.so.1]
+
+    // Read dynamic section
+    // Read dynamic section
+    try self_exe_file.seekTo(dynamic_symbols_table_offset);
+    const dynamic_symbol_count = dynamic_symbols_table_size / @sizeOf(std.elf.Elf64_Dyn);
+    const dynamic_symbols = try alloc.allocator.alloc(std.elf.Elf64_Dyn, dynamic_symbol_count);
+    defer alloc.allocator.free(dynamic_symbols);
+    _ = try self_exe_file.read(std.mem.sliceAsBytes(dynamic_symbols));
+
+    // Find string table address (DT_STRTAB)
+    var strtab_addr: u64 = 0;
+    for (dynamic_symbols) |dyn| {
+        if (dyn.d_tag == std.elf.DT_STRTAB) {
+            strtab_addr = dyn.d_val;
+            break;
+        }
+    }
+    if (strtab_addr == 0) {
+        print.printError("No string table found", .{});
+        return error.ElfStringsTableNotFound;
+    }
+
+    sections_header_iterator.index = 0;
+    var string_table_offset: u64 = 0;
+    while (try sections_header_iterator.next()) |shdr| {
+        if (shdr.sh_type == std.elf.SHT_STRTAB and shdr.sh_addr == strtab_addr) {
+            string_table_offset = shdr.sh_offset;
+            break;
+        }
+    }
+
+    if (string_table_offset == 0) {
+        // Fallback: Use program headers if section headers don’t map it
+        try self_exe_file.seekTo(elf_header.phoff);
+        const phdrs = try std.heap.page_allocator.alloc(std.elf.Elf64_Phdr, elf_header.phnum);
+        defer std.heap.page_allocator.free(phdrs);
+        _ = try self_exe_file.read(std.mem.sliceAsBytes(phdrs));
+        for (phdrs) |phdr| {
+            if (phdr.p_type == std.elf.PT_LOAD and phdr.p_vaddr <= strtab_addr and strtab_addr < phdr.p_vaddr + phdr.p_filesz) {
+                string_table_offset = phdr.p_offset + (strtab_addr - phdr.p_vaddr);
+                break;
+            }
+        }
+        if (string_table_offset == 0) {
+            print.printError("Could not map string table address", .{});
+            return error.ElfStringsTableNotFound;
+        }
+    }
+
+    for (dynamic_symbols) |dynamic_symbol| {
+        if (dynamic_symbol.d_tag == std.elf.DT_NULL) {
+            break;
+        }
+
+        if (dynamic_symbol.d_tag == std.elf.DT_NEEDED) {
+            const string_offset = string_table_offset + dynamic_symbol.d_val;
+            try self_exe_file.seekTo(string_offset);
+
+            // Read null-terminated string (up to 256 bytes max for simplicity)
+            var buffer: [256]u8 = undefined;
+            const bytes_read = try self_exe_file.read(&buffer);
+            const lib_name = buffer[0..bytes_read];
+
+            if (std.mem.indexOf(u8, lib_name, "musl")) |_| {
+                return LibCFlavor.MULSC;
+            }
+
+            if (std.mem.indexOf(u8, lib_name, "libc.so.6")) |_| {
+                return LibCFlavor.GNU_LIBC;
+            }
+        }
+    }
+
+    return LibCFlavor.UNKNOWN;
+}
---
+++ images/instrumentation/injector/src/jvm.zig
@@ -0,0 +1,275 @@
+// SPDX-FileCopyrightText: Copyright 2025 Dash0 Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+const std = @import("std");
+
+const alloc = @import("./allocator.zig");
+const print = @import("./print.zig");
+const res_attrs = @import("./resource_attributes.zig");
+const types = @import("./types.zig");
+
+const testing = std.testing;
+
+const otel_java_agent_path = "/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar";
+const javaagent_flag_value = "-javaagent:" ++ otel_java_agent_path;
+
+pub fn checkOTelJavaAgentJarAndGetModifiedJavaToolOptionsValue(name: [:0]const u8, original_value: ?[:0]const u8) ?types.NullTerminatedString {
+    // Check the existence of the Jar file: by passing a `-javaagent` to a
+    // jar file that does not exist or cannot be opened will crash the JVM
+    std.fs.cwd().access(otel_java_agent_path, .{}) catch |err| {
+        print.printError("Skipping injection of OTel Java Agent in 'JAVA_TOOL_OPTIONS' because of an issue accessing the Jar file at {s}: {}", .{ otel_java_agent_path, err });
+        return null;
+    };
+
+    const resource_attributes_optional: ?[]u8 = res_attrs.getResourceAttributes();
+    return getModifiedJavaToolOptionsValue(
+        name,
+        original_value,
+        resource_attributes_optional,
+    );
+}
+
+/// Returns the modified value for JAVA_TOOL_OPTIONS, including the -javaagent flag; based on the original value of
+/// JAVA_TOOL_OPTIONS and the provided resource attributes that should be added.
+///
+/// Do no deallocate the return value, or we may cause a USE_AFTER_FREE memory corruption in the parent process.
+///
+/// getModifiedJavaToolOptionsValue will free the new_resource_attributes_optional parameter if it is not null
+fn getModifiedJavaToolOptionsValue(
+    name: [:0]const u8,
+    original_value_optional: ?[:0]const u8,
+    new_resource_attributes_optional: ?[]u8,
+) ?types.NullTerminatedString {
+    // For auto-instrumentation, we inject the -javaagent flag into the JAVA_TOOL_OPTIONS environment variable. In
+    // addition, we use JAVA_TOOL_OPTIONS to supply addtional resource attributes. The Java runtime does not look up the
+    // OTEL_RESOURCE_ATTRIBUTES environment variable using getenv(), instead it parses the environment block
+    // /proc/env/<pid> directly. We cannot hook into mechanism to introduce additional resource attributes. Instead, we
+    // add them together with the -javaagent flag as the -Dotel.resource.attributes Java system property to
+    // JAVA_TOOL_OPTIONS. If the -Dotel.resource.attributes system property already exists in the original
+    // JAVA_TOOL_OPTIONS value, we need to merge the two list of key-value pairs. If -Dotel.resource.attributes is
+    // supplied via other means (for example via the command line), the value from the command line will override the
+    // value we add here to JAVA_TOOL_OPTIONS, which can be verified as follows:
+    //     % JAVA_TOOL_OPTIONS="-Dprop=B" jshell -R -Dprop=A
+    //     Picked up JAVA_TOOL_OPTIONS: -Dprop=B
+    //     jshell> System.getProperty("prop")
+    //     $1 ==> "A"
+    if (original_value_optional) |original_value| {
+        // If JAVA_TOOL_OPTIONS is already set, append our values.
+        if (new_resource_attributes_optional) |new_resource_attributes| {
+            defer alloc.allocator.free(new_resource_attributes);
+
+            if (std.mem.indexOf(u8, original_value, "-Dotel.resource.attributes=")) |startIdx| {
+                // JAVA_TOOL_OPTIONS already contains -Dotel.resource.attribute, we need to merge the existing with the new key-value list.
+                const actualStartIdx = startIdx + 27;
+                // assume the list of key-value pairs is terminated by a space of the end of the string
+                var originalKvPairs: []const u8 = "";
+                var remainingJavaToolOptions: []const u8 = "";
+                if (std.mem.indexOfPos(u8, original_value, actualStartIdx, " ")) |endIdx| {
+                    originalKvPairs = original_value[actualStartIdx..endIdx];
+                    remainingJavaToolOptions = original_value[endIdx..];
+                } else {
+                    originalKvPairs = original_value[actualStartIdx..];
+                    remainingJavaToolOptions = "";
+                }
+
+                const mergedKvPairs = std.fmt.allocPrintZ(alloc.allocator, "{s},{s}", .{
+                    originalKvPairs,
+                    new_resource_attributes,
+                }) catch |err| {
+                    print.printError("Cannot allocate memory to manipulate the value of '{s}': {}", .{ name, err });
+                    // TODO should we actually return null here? I think it should be the original value instead.
+                    return null;
+                };
+                defer alloc.allocator.free(mergedKvPairs);
+                const return_buffer =
+                    std.fmt.allocPrintZ(alloc.allocator, "{s}-Dotel.resource.attributes={s}{s} {s}", .{
+                        original_value[0..startIdx],
+                        mergedKvPairs,
+                        remainingJavaToolOptions,
+                        javaagent_flag_value,
+                    }) catch |err| {
+                        print.printError("Cannot allocate memory to manipulate the value of '{s}': {}", .{ name, err });
+                        // TODO should we actually return null here? I think it should be the original value instead.
+                        return null;
+                    };
+                return return_buffer.ptr;
+            }
+
+            // JAVA_TOOL_OPTIONS is set but does not contain does not already contain -Dotel.resource.attributes, and
+            // new resource attributes have been provided. Add -javaagent and -Dotel.resource.attributes.
+            const return_buffer =
+                std.fmt.allocPrintZ(alloc.allocator, "{s} {s} -Dotel.resource.attributes={s}", .{
+                    original_value,
+                    javaagent_flag_value,
+                    new_resource_attributes,
+                }) catch |err| {
+                    print.printError("Cannot allocate memory to manipulate the value of '{s}': {}", .{ name, err });
+                    // TODO should we actually return null here? I think it should be the original value instead.
+                    return null;
+                };
+            return return_buffer.ptr;
+        } else {
+            // JAVA_TOOL_OPTIONS is set, but no new resource attributes have been provided.
+            const return_buffer =
+                std.fmt.allocPrintZ(alloc.allocator, "{s} {s}", .{
+                    original_value,
+                    javaagent_flag_value,
+                }) catch |err| {
+                    print.printError("Cannot allocate memory to manipulate the value of '{s}': {}", .{ name, err });
+                    // TODO should we actually return null here? I think it should be the original value instead.
+                    return null;
+                };
+            return return_buffer.ptr;
+        }
+    } else {
+        // JAVA_TOOL_OPTIONS is not set, but new resource attributes have been provided.
+        if (new_resource_attributes_optional) |new_resource_attributes| {
+            defer alloc.allocator.free(new_resource_attributes);
+            const return_buffer = std.fmt.allocPrintZ(alloc.allocator, "{s} -Dotel.resource.attributes={s}", .{
+                javaagent_flag_value,
+                new_resource_attributes,
+            }) catch |err| {
+                print.printError("Cannot allocate memory to manipulate the value of '{s}': {}", .{ name, err });
+                // TODO should we actually return null here? I think it should be the original value instead.
+                return null;
+            };
+            return return_buffer.ptr;
+        } else {
+            // JAVA_TOOL_OPTIONS is not set, and no new resource attributes have been provided. Simply return the -javaagent flag.
+            return javaagent_flag_value[0..].ptr;
+        }
+    }
+}
+
+test "getModifiedJavaToolOptionsValue: should return -javaagent if original value is unset and no resource attributes are provided" {
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        null,
+        null,
+    );
+    try testing.expectEqualStrings(
+        "-javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should return -javaagent and -Dotel.resource.attributes if original value is unset and resource attributes are provided" {
+    const resource_attributes: []u8 = try alloc.allocator.alloc(u8, 15);
+    var fbs = std.io.fixedBufferStream(resource_attributes);
+    _ = try fbs.writer().write("aaa=bbb,ccc=ddd");
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        null,
+        resource_attributes,
+    );
+    try testing.expectEqualStrings(
+        "-javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar -Dotel.resource.attributes=aaa=bbb,ccc=ddd",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should append -javaagent if original value exists and no resource attributes are provided" {
+    const original_value: [:0]const u8 = "-Dsome.property=value"[0.. :0];
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        original_value,
+        null,
+    );
+    try testing.expectEqualStrings(
+        "-Dsome.property=value -javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should append -javaagent if original value exists and has -Dotel.resource.attributes but no new resource attributes are provided, " {
+    const original_value: [:0]const u8 = "-Dsome.property=value -Dotel.resource.attributes=www=xxx,yyy=zzz"[0.. :0];
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        original_value,
+        null,
+    );
+    try testing.expectEqualStrings(
+        "-Dsome.property=value -Dotel.resource.attributes=www=xxx,yyy=zzz -javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should append -javaagent and -Dotel.resource.attributes if original value exists and resource attributes are provided" {
+    const original_value: [:0]const u8 = "-Dsome.property=value"[0.. :0];
+    const resource_attributes: []u8 = try alloc.allocator.alloc(u8, 15);
+    var fbs = std.io.fixedBufferStream(resource_attributes);
+    _ = try fbs.writer().write("aaa=bbb,ccc=ddd");
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        original_value,
+        resource_attributes,
+    );
+    try testing.expectEqualStrings(
+        "-Dsome.property=value -javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar -Dotel.resource.attributes=aaa=bbb,ccc=ddd",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should merge new and existing -Dotel.resource.attributes (only property)" {
+    const original_value: [:0]const u8 = "-Dotel.resource.attributes=eee=fff,ggg=hhh"[0.. :0];
+    const resource_attributes: []u8 = try alloc.allocator.alloc(u8, 15);
+    var fbs = std.io.fixedBufferStream(resource_attributes);
+    _ = try fbs.writer().write("aaa=bbb,ccc=ddd");
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        original_value,
+        resource_attributes,
+    );
+    try testing.expectEqualStrings(
+        "-Dotel.resource.attributes=eee=fff,ggg=hhh,aaa=bbb,ccc=ddd -javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should merge new and existing -Dotel.resource.attributes (at the start)" {
+    const original_value: [:0]const u8 = "-Dproperty1=value -Dotel.resource.attributes=eee=fff,ggg=hhh"[0.. :0];
+    const resource_attributes: []u8 = try alloc.allocator.alloc(u8, 15);
+    var fbs = std.io.fixedBufferStream(resource_attributes);
+    _ = try fbs.writer().write("aaa=bbb,ccc=ddd");
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        original_value,
+        resource_attributes,
+    );
+    try testing.expectEqualStrings(
+        "-Dproperty1=value -Dotel.resource.attributes=eee=fff,ggg=hhh,aaa=bbb,ccc=ddd -javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should merge new and existing -Dotel.resource.attributes (in the middle)" {
+    const original_value: [:0]const u8 = "-Dproperty1=value -Dotel.resource.attributes=eee=fff,ggg=hhh -Dproperty2=value"[0.. :0];
+    const resource_attributes: []u8 = try alloc.allocator.alloc(u8, 15);
+    var fbs = std.io.fixedBufferStream(resource_attributes);
+    _ = try fbs.writer().write("aaa=bbb,ccc=ddd");
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        original_value,
+        resource_attributes,
+    );
+    try testing.expectEqualStrings(
+        "-Dproperty1=value -Dotel.resource.attributes=eee=fff,ggg=hhh,aaa=bbb,ccc=ddd -Dproperty2=value -javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
+
+test "getModifiedJavaToolOptionsValue: should merge new and existing -Dotel.resource.attributes (at the end)" {
+    const original_value: [:0]const u8 = "-Dproperty1=value -Dotel.resource.attributes=eee=fff,ggg=hhh"[0.. :0];
+    const resource_attributes: []u8 = try alloc.allocator.alloc(u8, 15);
+    var fbs = std.io.fixedBufferStream(resource_attributes);
+    _ = try fbs.writer().write("aaa=bbb,ccc=ddd");
+    const modifiedJavaToolOptions = getModifiedJavaToolOptionsValue(
+        "JAVA_TOOL_OPTIONS",
+        original_value,
+        resource_attributes,
+    );
+    try testing.expectEqualStrings(
+        "-Dproperty1=value -Dotel.resource.attributes=eee=fff,ggg=hhh,aaa=bbb,ccc=ddd -javaagent:/__dash0__/instrumentation/jvm/opentelemetry-javaagent.jar",
+        std.mem.span(modifiedJavaToolOptions orelse "-"),
+    );
+}
