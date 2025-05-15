93888c141a0c15ca2844fe8abc12f69268ce26a6
Bastian Krol <bastian.krol@dash0.com>
2025-05-15 17:50:01 +0200

    whatever :)

--- images/instrumentation/injector/src/root.zig
+++ images/instrumentation/injector/src/root.zig
@@ -25,10 +25,7 @@ const environ_buffer_ptr: *[131072]u8 = &environ_buffer;
 
 // Export __environ as a strong symbol, to override the glibc/musl symbol.
 export var __environ: [*]u8 = environ_buffer_ptr;
-
-var my_environ_buffer: [131072]u8 = [_]u8{0} ** 131072;
-const my_environ_buffer_ptr: *[131072]u8 = &my_environ_buffer;
-export var __my_environ: [*]u8 = my_environ_buffer_ptr;
+// export var __my_environ: [*]u8 = environ_buffer_ptr;
 
 // Also declare (and thus override) the aliases _environ and environ, see e.g.
 // https://sourceware.org/git/?p=glibc.git;a=blob;f=posix/environ.c
@@ -68,7 +65,9 @@ fn initEnviron() callconv(.C) void {
 }
 
 fn _initEnviron() !void {
-    std.debug.print("root.zig#_initEnviron() START\n", .{});
+    // std.debug.print("root.zig#_initEnviron() START\n", .{});
+    // std.debug.print("root.zig#_initEnviron: XXX my_exported pointer: {x}\n", .{@intFromPtr(my_exported)});
+
     const proc_self_environ_path = "/proc/self/environ";
     const proc_self_environ_file = try std.fs.openFileAbsolute(proc_self_environ_path, .{
         .mode = std.fs.File.OpenMode.read_only,
@@ -81,31 +80,27 @@ fn _initEnviron() !void {
     // StackFallbackAllocator might be a good convenient option.
     var buf_reader = std.io.bufferedReader(proc_self_environ_file.reader());
     var in_stream = buf_reader.reader();
-    std.debug.print("root.zig#_initEnviron() init in_stream done\n", .{});
+    // std.debug.print("root.zig#_initEnviron() init in_stream done\n", .{});
 
     // We have not seen OTEL_RESOURCE_ATTRIBUTES /proc/self/environ, add it now.
     for (res_attrs.otel_resource_attributes_env_var_name) |c| {
         environ_buffer[environ_idx] = c;
         environ_idx += 1;
-        my_environ_buffer[environ_idx] = c;
-        environ_idx += 1;
     }
     environ_buffer[environ_idx] = '=';
-    my_environ_buffer[environ_idx] = '=';
     environ_idx += 1;
     for ("k8s.namespace.name=my-namespace,k8s.pod.name=my-pod,k8s.pod.uid=275ecb36-5aa8-4c2a-9c47-d8bb681b9aff,k8s.container.name=test-app") |c| {
         environ_buffer[environ_idx] = c;
         environ_idx += 1;
     }
     environ_buffer[environ_idx] = 0;
-    my_environ_buffer[environ_idx] = 0;
     environ_idx += 1;
-    std.debug.print("root.zig#_initEnviron() OTEL_RESOURCE_ATTRIBUTES has been added\n", .{});
+    // std.debug.print("root.zig#_initEnviron() OTEL_RESOURCE_ATTRIBUTES has been added\n", .{});
 
     var init_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer init_arena.deinit();
     const init_allocator = init_arena.allocator();
-    std.debug.print("root.zig#_initEnviron() init init_allocator done\n", .{});
+    // std.debug.print("root.zig#_initEnviron() init init_allocator done\n", .{});
     // TODO max_size? We currently set std.math.maxInt(usize) which basically means "unlimited", we might want to set
     // an (arbitrary but reasonable) limit here?
     // TODO switch to local var again var environ_idx: usize = 0;
@@ -113,71 +108,67 @@ fn _initEnviron() !void {
         // std.debug.print("root.zig#_initEnviron: read environ entry: {s}\n", .{environ_entry});
         for (environ_entry) |c| {
             environ_buffer[environ_idx] = c;
-            my_environ_buffer[environ_idx] = c;
             environ_idx += 1;
         }
         // add null terminator for this environ entry
         environ_buffer[environ_idx] = 0;
-        my_environ_buffer[environ_idx] = 0;
         environ_idx += 1;
     }
-    std.debug.print("root.zig#_initEnviron() /proc/self/environ has been read once\n", .{});
+    // std.debug.print("root.zig#_initEnviron() /proc/self/environ has been read once\n", .{});
 
     // Add final null terminator to signify the end of __environ.
     environ_buffer[environ_idx] = 0;
-    my_environ_buffer[environ_idx] = 0;
     environ_idx += 1;
 
     std.debug.print("root.zig#_initEnviron: XXX __environ outer pointer: {x}\n", .{@intFromPtr(__environ)});
-    std.debug.print("root.zig#_initEnviron: XXX __environ inner pointer: {x}\n", .{@intFromPtr(environ_buffer_ptr)});
     std.debug.print("root.zig#_initEnviron: XXX environ_buffer: {s}\n", .{ environ_buffer[0..environ_idx] });
-    std.debug.print("root.zig#_initEnviron: XXX __my_environ outer pointer: {x}\n", .{@intFromPtr(__my_environ)});
-    std.debug.print("root.zig#_initEnviron: XXX __my_environ inner pointer: {x}\n", .{@intFromPtr(my_environ_buffer_ptr)});
-    std.debug.print("root.zig#_initEnviron: XXX my_environ_buffer: {s}\n", .{ my_environ_buffer[0..environ_idx] });
-    std.debug.print("root.zig#_initEnviron() END\n", .{});
+    // std.debug.print("root.zig#_initEnviron: XXX __my_environ outer pointer: {x}\n", .{@intFromPtr(__my_environ)});
+    // std.debug.print("root.zig#_initEnviron: XXX __my_environ inner pointer: {x}\n", .{@intFromPtr(my_environ_buffer_ptr)});
+    // std.debug.print("root.zig#_initEnviron: XXX my_environ_buffer: {s}\n", .{ my_environ_buffer[0..environ_idx] });
+    // std.debug.print("root.zig#_initEnviron() END\n", .{});
 }
 
-export fn getenv(name_z: types.NullTerminatedString) ?types.NullTerminatedString {
-    const name = std.mem.sliceTo(name_z, 0);
-
-    std.debug.print("root.zig#getenv: XXX __environ outer pointer: {x}\n", .{@intFromPtr(__environ)});
-    // std.debug.print("root.zig#getenv: XXX __environ inner pointer: {x}\n", .{@intFromPtr(__environ.ptr)});
-    std.debug.print("root.zig#getenv: XXX environ_buffer: {s}\n", .{ environ_buffer[0..environ_idx] });
-
-    // TODO if we do not modify stuff because we do not override putenv and friends, we might get rid of the mutex.
-    // Need to change type from `const` to be able to lock
-    var env_mutex = _env_mutex;
-    env_mutex.lock();
-    defer env_mutex.unlock();
-
-    // Dynamic libs do not get the std.os.environ initialized, see https://github.com/ziglang/zig/issues/4524, so we
-    // back fill it. This logic is based on parsing of envp on zig's start. We re-bind the environment every time, as we
-    // cannot ensure it did not change since the previous invocation. Libc implementations can re-allocate the
-    // environment (http://github.com/lattera/glibc/blob/master/stdlib/setenv.c;
-    // https://git.musl-libc.org/cgit/musl/tree/src/env/setenv.c) if the backing memory location is outgrown by apps
-    // modifying the environment via setenv or putenv.
-    const environment_optional: [*:null]?[*:0]u8 = @ptrCast(@alignCast(__environ));
-    var environment_count: usize = 0;
-    if (@intFromPtr(__environ) != 0) { // __environ can be a null pointer, e.g. directly after clearenv()
-        while (environment_optional[environment_count]) |_| : (environment_count += 1) {}
-    }
-    std.os.environ = @as([*][*:0]u8, @ptrCast(environment_optional))[0..environment_count];
-
-    // Technically, a process could change the value of `DASH0_INJECTOR_DEBUG` after it started (mostly when we debug
-    // stuff in REPL) so we look up the value every time.
-    print.initDebugFlag();
-    print.printDebug("getenv({s}) called", .{name});
-
-    const res = getEnvValue(name);
-
-    if (res) |value| {
-        print.printDebug("getenv({s}) -> '{s}'", .{ name, value });
-    } else {
-        print.printDebug("getenv({s}) -> null", .{name});
-    }
-
-    return res;
-}
+// fn getenv(name_z: types.NullTerminatedString) ?types.NullTerminatedString {
+//     const name = std.mem.sliceTo(name_z, 0);
+//
+//     std.debug.print("root.zig#getenv: XXX __environ outer pointer: {x}\n", .{@intFromPtr(__environ)});
+//     // std.debug.print("root.zig#getenv: XXX __environ inner pointer: {x}\n", .{@intFromPtr(__environ.ptr)});
+//     // std.debug.print("root.zig#getenv: XXX environ_buffer: {s}\n", .{__environ[0..environ_idx]});
+//
+//     // TODO if we do not modify stuff because we do not override putenv and friends, we might get rid of the mutex.
+//     // Need to change type from `const` to be able to lock
+//     var env_mutex = _env_mutex;
+//     env_mutex.lock();
+//     defer env_mutex.unlock();
+//
+//     // Dynamic libs do not get the std.os.environ initialized, see https://github.com/ziglang/zig/issues/4524, so we
+//     // back fill it. This logic is based on parsing of envp on zig's start. We re-bind the environment every time, as we
+//     // cannot ensure it did not change since the previous invocation. Libc implementations can re-allocate the
+//     // environment (http://github.com/lattera/glibc/blob/master/stdlib/setenv.c;
+//     // https://git.musl-libc.org/cgit/musl/tree/src/env/setenv.c) if the backing memory location is outgrown by apps
+//     // modifying the environment via setenv or putenv.
+//     const environment_optional: [*:null]?[*:0]u8 = @ptrCast(@alignCast(__environ));
+//     var environment_count: usize = 0;
+//     if (@intFromPtr(__environ) != 0) { // __environ can be a null pointer, e.g. directly after clearenv()
+//         while (environment_optional[environment_count]) |_| : (environment_count += 1) {}
+//     }
+//     std.os.environ = @as([*][*:0]u8, @ptrCast(environment_optional))[0..environment_count];
+//
+//     // Technically, a process could change the value of `DASH0_INJECTOR_DEBUG` after it started (mostly when we debug
+//     // stuff in REPL) so we look up the value every time.
+//     print.initDebugFlag();
+//     print.printDebug("getenv({s}) called", .{name});
+//
+//     const res = getEnvValue(name);
+//
+//     if (res) |value| {
+//         print.printDebug("getenv({s}) -> '{s}'", .{ name, value });
+//     } else {
+//         print.printDebug("getenv({s}) -> null", .{name});
+//     }
+//
+//     return res;
+// }
 
 fn getEnvValue(name: [:0]const u8) ?types.NullTerminatedString {
     const original_value = std.posix.getenv(name);
