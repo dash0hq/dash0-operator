9001130a532f760cf7cb1846c377b62501c0d8a9
Bastian Krol <bastian.krol@dash0.com>
2025-05-18 12:02:21 +0200

    test(injector): add Node.js test setup to experiments
    [skip ci]

--- images/instrumentation/injector-experiments/zig-to-nodejs/01_export__environ/rebuild-and-run.sh
+++ images/instrumentation/injector-experiments/zig-to-nodejs/01_export__environ/rebuild-and-run.sh
@@ -7,15 +7,32 @@ set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+node_sources=/home/dash0/instrumentation/injector-experiments/third-party/node
+
+# Use installed Node.js binary:
+# node_js=node
+
+# Use locally built Node.js binary (release build):
+# node_js="$node_sources"/out/Release/node
+# Use locally built Node.js binary (debug build):
+node_js="$node_sources"/out/Debug/node
+
+if [[ "$node_js" =~ "out/Release" ]]; then
+  echo "Using locally built Node.js binary, rebuiding Node.js"
+  pushd "$node_sources" > /dev/null
+  ./configure --debug
+  make -j4
+  popd > /dev/null
+fi
+
 echo building zig
 zig build
 echo zig build
 
-echo
-echo accessing specific env vars:
-LD_PRELOAD=./libsymbols.so node -e 'console.log("PATH:", process.env.PATH); console.log("VAR3:", process.env.VAR3); console.log("VAR4:", process.env.VAR4);'
+# $ gdb /opt/node-debug/node core.node.8.1535359906
+# (gdb) backtrace
 
-# !! This segfaults:
 echo
-echo accessing raw process.env segfaults:
-LD_PRELOAD=./libsymbols.so node -e 'console.log(process.env);'
+set -x
+# LD_PRELOAD=./libsymbols.so /home/dash0/instrumentation/injector-experiments/third-party/node/out/Release/node script.js
+LD_PRELOAD=./libsymbols.so "$node_js" --no-node-snapshot script.js
