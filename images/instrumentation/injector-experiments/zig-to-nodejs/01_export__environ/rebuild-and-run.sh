ea22eaaf6587d3feb22aa21094a6f4e3ab9bb699
Bastian Krol <bastian.krol@dash0.com>
2025-05-19 08:41:45 +0200

    test(injector): add JDK test setup to experiments
    [skip ci]

--- images/instrumentation/injector-experiments/zig-to-nodejs/01_export__environ/rebuild-and-run.sh
+++ images/instrumentation/injector-experiments/zig-to-nodejs/01_export__environ/rebuild-and-run.sh
@@ -10,18 +10,26 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 node_sources=/home/dash0/instrumentation/injector-experiments/third-party/node
 
 # Use installed Node.js binary:
-# node_js=node
+node_js=node
 
 # Use locally built Node.js binary (release build):
 # node_js="$node_sources"/out/Release/node
 # Use locally built Node.js binary (debug build):
-node_js="$node_sources"/out/Debug/node
+# node_js="$node_sources"/out/Debug/node
 
-if [[ "$node_js" =~ "out/Release" ]]; then
+if [[ "$node_js" =~ "/out/" ]]; then
   echo "Using locally built Node.js binary, rebuiding Node.js"
   pushd "$node_sources" > /dev/null
-  ./configure --debug
-  make -j4
+  # ./configure
+  # ./configure --debug
+  # Building the whole Node.js code base in the container was somewhat finicky, building with just one parallel job
+  # (no -j) was abysmally slow, but running with -j4 crashed in linking steps due to out of memory (I believe):
+  #   LINK(target) /home/dash0/instrumentation/injector-experiments/third-party/node/out/Debug/node_mksnapshot
+  #   collect2: fatal error: ld terminated with signal 9 [Killed]
+  #   compilation terminated.
+  # A good compromise might be to run an initial build with -j4, then finish up with -j2 or without -j.
+  # Giving the Docker VM more memory might help.
+  time make -j8
   popd > /dev/null
 fi
 
@@ -29,10 +37,6 @@ echo building zig
 zig build
 echo zig build
 
-# $ gdb /opt/node-debug/node core.node.8.1535359906
-# (gdb) backtrace
-
 echo
 set -x
-# LD_PRELOAD=./libsymbols.so /home/dash0/instrumentation/injector-experiments/third-party/node/out/Release/node script.js
-LD_PRELOAD=./libsymbols.so "$node_js" --no-node-snapshot script.js
+LD_PRELOAD=./libsymbols.so "$node_js" script.js
