a017838ad9d87e21d924c8660054f16282d312eb
Bastian Krol <bastian.krol@dash0.com>
2025-05-19 08:41:45 +0200

    test(injector): add JDK test setup to experiments
    [skip ci]

--- images/instrumentation/injector-experiments/zig-to-nodejs/01_export__environ/script.js
+++ images/instrumentation/injector-experiments/zig-to-nodejs/01_export__environ/script.js
@@ -1,7 +1,21 @@
-console.log("Accessing individual environment variables works:");
+console.log("accessing individual environment variables:");
 console.log("PATH:", process.env.PATH);
 console.log("VAR3:", process.env.VAR3);
 console.log("VAR4:", process.env.VAR4);
 
-console.log("Now accessing process.env, this will segfault");
+console.log("accessing process.env");
+console.log(process.env);
+
+console.log("writing to process.env");
+process.env.NEW_VAR1 = "new value 1";
+process.env.NEW_VAR2 = "new value 2";
+
+console.log("accessing individual environment variables:");
+console.log("PATH:", process.env.PATH);
+console.log("VAR3:", process.env.VAR3);
+console.log("VAR4:", process.env.VAR4);
+console.log("NEW_VAR1:", process.env.NEW_VAR1);
+console.log("NEW_VAR2:", process.env.NEW_VAR2);
+
+console.log("accessing process.env");
 console.log(process.env);
