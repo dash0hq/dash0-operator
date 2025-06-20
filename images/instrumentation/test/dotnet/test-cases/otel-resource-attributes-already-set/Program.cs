5e02e5f892e4611101feadf671f7926e0beb6bdb
Bastian Krol <bastian.krol@dash0.com>
2025-06-21 01:22:50 +0200

    add more .NET instrumentation image integration tests
    [skip ci]

---
+++ images/instrumentation/test/dotnet/test-cases/otel-resource-attributes-already-set/Program.cs
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: Copyright 2025 Dash0 Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+namespace Dash0;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        verifyEnvVar("OTEL_RESOURCE_ATTRIBUTES", "k8s.namespace.name=namespace,k8s.pod.name=pod_name,k8s.pod.uid=pod_uid,k8s.container.name=container_name,key1=value1,key2=value2");
+        return 0;
+    }
+
+    private static void verifyEnvVar(string envVarName, string expectedValue)
+    {
+        string? actualValue = Environment.GetEnvironmentVariable(envVarName);
+        if (expectedValue == null)
+        {
+        	if (actualValue != null)
+        	{
+				Console.Error.WriteLine($"Unexpected value for {envVarName}: expected: null, was: \"{actualValue}\"");
+				System.Environment.Exit(1);
+			}
+			else
+			{
+				Console.Error.WriteLine($"Expected value for {envVarName}: null.");
+				return;
+			}
+        }
+        if (actualValue == null)
+        {
+            Console.Error.WriteLine($"Unexpected value for {envVarName}: expected: \"{expectedValue}\", was: null");
+            System.Environment.Exit(1);
+        }
+        else if (actualValue != expectedValue)
+        {
+            Console.Error.WriteLine($"Unexpected value for {envVarName}: expected: \"{expectedValue}\", was: \"{actualValue}\"");
+            System.Environment.Exit(1);
+        }
+        else
+        {
+            Console.Error.WriteLine($"Expected value for {envVarName}: \"{actualValue}\"");
+        }
+    }
+}
