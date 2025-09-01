d1af270ef8daf5a6c5df94ffe85d33b7f18eb0ed
Michele Mancioppi <michele.mancioppi@dash0.com>
2025-09-01 19:42:47 +0200

    remove link-time dependency from __environ and setenv
    The injector may be linked to binaries that, while dynamically
    linked, do not link a LibC flavor. This results on the injector
    preventing the program from starting due to a linker error.
    
    This change introduces a way to look up at runtime, in an
    initialization step, the location in the process' memory of
    the __environ and setenv symbols, so that we can use them
    to read and manipulate the process environment without depending
    on them at link time.
    
    The way we do this is in three steps:
    1. Find out which libc the process has (if any) -- We already did
       this read Elf metadata (DT_NEEDED) as some OpenTelemetry SDKs
       need to be injected differently (using different binaries)
       depending on the LibC flavor.
    2. In the loaded libc, find the location of the `dlsym` function
       reading Elf metadata. To accomplish this, the change "forks"
       the ElfDynLib from `dynamic_library` and modifies it to remove
       stuff we don’t care about (e.g., symbol version matching), and
       to prevent it from memory mapping (as what we need was already
       mapped in memory by the process loading); think of it as “just
       enough Elf to find a symbol the process already has”; as a side
       note, it’s fascinating that, by casting memory locations, we can
       achieve the lookup without a single allocation.
    3. Let the LibC's `dlsym` handle the fifty quintillion corner cases
       (aliases, lazy loading, linker resolution order, `__environ`
       exported by the main binary, etc.) and find us `__environ` and
       `setenv`

--- images/instrumentation/injector/src/root.zig
+++ images/instrumentation/injector/src/root.zig
@@ -4,6 +4,7 @@
 const std = @import("std");
 
 const dotnet = @import("dotnet.zig");
+const libc = @import("libc.zig");
 const jvm = @import("jvm.zig");
 const node_js = @import("node_js.zig");
 const print = @import("print.zig");
@@ -11,12 +12,9 @@ const res_attrs = @import("resource_attributes.zig");
 const types = @import("types.zig");
 
 const assert = std.debug.assert;
-const testing = std.testing;
-const expect = testing.expect;
+const expect = std.testing.expect;
 
-// We need to use a rather "innocent" type here, the actual type involves
-// optionals that cannot be used in global declarations.
-extern var __environ: [*]u8;
+export const init_array: [1]*const fn () callconv(.C) void linksection(".init_array") = .{&initEnviron};
 
 // Ensure we process requests synchronously. LibC is *not* threadsafe
 // with respect to the environment, but chances are some apps will try
@@ -29,8 +27,62 @@ var modified_java_tool_options_value_calculated = false;
 var modified_java_tool_options_value: ?types.NullTerminatedString = null;
 var modified_node_options_value_calculated = false;
 var modified_node_options_value: ?types.NullTerminatedString = null;
-var modified_otel_resource_attributes_value_calculated = false;
-var modified_otel_resource_attributes_value: ?types.NullTerminatedString = null;
+
+var environ_ptr: ?types.EnvironPtr = null;
+var setenv_fn_ptr: ?types.SetenvFnPtr = null;
+
+const InjectorError = error{
+    CannotFindEnvironSymbol,
+};
+
+fn initEnviron() callconv(.C) void {
+    const libc_library = libc.getLibc() catch |err| {
+        if (err == error.UnknownLibCFlavor) {
+            print.printDebug("no LibC found: {}", .{err});
+        } else {
+            print.printError("failed to identify libc: {}", .{err});
+        }
+        return;
+    };
+
+    print.printDebug("identified {s} LibC loaded from {s}", .{ switch (libc_library.flavor) {
+        types.LibCFlavor.GNU => "GNU",
+        types.LibCFlavor.MUSL => "musl",
+        else => "unknown",
+    }, libc_library.name });
+
+    environ_ptr = libc_library.environ_ptr;
+
+    updateStdOsEnviron() catch |err| {
+        print.printError("cannot initiailize OTEL_RESOURCE_ATTRIBUTES: {}", .{err});
+        return;
+    };
+
+    if (res_attrs.getModifiedOtelResourceAttributesValue(std.posix.getenv(res_attrs.otel_resource_attributes_env_var_name))) |otel_resource_attributes_value| {
+        const setenv_res = libc_library.setenv_fn_ptr(res_attrs.otel_resource_attributes_env_var_name, otel_resource_attributes_value, true);
+        if (setenv_res != 0) {
+            print.printError("failed to set modified value for '{s}' to '{s}': errno={}", .{ res_attrs.otel_resource_attributes_env_var_name, otel_resource_attributes_value, setenv_res });
+        }
+    }
+}
+
+fn updateStdOsEnviron() !void {
+    // Dynamic libs do not get the std.os.environ initialized, see https://github.com/ziglang/zig/issues/4524, so we
+    // back fill it. This logic is based on parsing of envp on zig's start. We re-bind the environment every time, as
+    // we cannot ensure it did not change since the previous invocation. Libc implementations can re-allocate the
+    // environment (http://github.com/lattera/glibc/blob/master/stdlib/setenv.c;
+    // https://git.musl-libc.org/cgit/musl/tree/src/env/setenv.c) if the backing memory location is outgrown by apps
+    // modifying the environment via setenv or putenv.
+    if (environ_ptr) |environment_ptr| {
+        const env_array = environment_ptr.*;
+        var env_var_count: usize = 0;
+        while (env_array[env_var_count] != null) : (env_var_count += 1) {}
+
+        std.os.environ = @ptrCast(@constCast(env_array[0..env_var_count]));
+    } else {
+        return error.CannotFindEnvironSymbol;
+    }
+}
 
 export fn getenv(name_z: types.NullTerminatedString) ?types.NullTerminatedString {
     const name = std.mem.sliceTo(name_z, 0);
@@ -40,30 +92,23 @@ export fn getenv(name_z: types.NullTerminatedString) ?types.NullTerminatedString
     env_mutex.lock();
     defer env_mutex.unlock();
 
-    // Dynamic libs do not get the std.os.environ initialized, see https://github.com/ziglang/zig/issues/4524, so we
-    // back fill it. This logic is based on parsing of envp on zig's start. We re-bind the environment every time, as we
-    // cannot ensure it did not change since the previous invocation. Libc implementations can re-allocate the
-    // environment (http://github.com/lattera/glibc/blob/master/stdlib/setenv.c;
-    // https://git.musl-libc.org/cgit/musl/tree/src/env/setenv.c) if the backing memory location is outgrown by apps
-    // modifying the environment via setenv or putenv.
-    const environment_optional: [*:null]?[*:0]u8 = @ptrCast(@alignCast(__environ));
-    var environment_count: usize = 0;
-    if (@intFromPtr(__environ) != 0) { // __environ can be a null pointer, e.g. directly after clearenv()
-        while (environment_optional[environment_count]) |_| : (environment_count += 1) {}
-    }
-    std.os.environ = @as([*][*:0]u8, @ptrCast(environment_optional))[0..environment_count];
+    print.initDebugFlag();
+    print.printDebug("getenv('{s}') called", .{name});
+
+    updateStdOsEnviron() catch |err| {
+        print.printDebug("getenv('{s}') -> null; could not update std.os.environ: {}; ", .{ name, err });
+        return null;
+    };
 
     // Technically, a process could change the value of `DASH0_INJECTOR_DEBUG` after it started (mostly when we debug
     // stuff in REPL) so we look up the value every time.
-    print.initDebugFlag();
-    print.printDebug("getenv({s}) called", .{name});
 
     const res = getEnvValue(name);
 
     if (res) |value| {
-        print.printDebug("getenv({s}) -> '{s}'", .{ name, value });
+        print.printDebug("getenv('{s}') -> '{s}'", .{ name, value });
     } else {
-        print.printDebug("getenv({s}) -> null", .{name});
+        print.printDebug("getenv('{s}') -> null", .{name});
     }
 
     return res;
@@ -72,19 +117,7 @@ export fn getenv(name_z: types.NullTerminatedString) ?types.NullTerminatedString
 fn getEnvValue(name: [:0]const u8) ?types.NullTerminatedString {
     const original_value = std.posix.getenv(name);
 
-    if (std.mem.eql(
-        u8,
-        name,
-        res_attrs.otel_resource_attributes_env_var_name,
-    )) {
-        if (!modified_otel_resource_attributes_value_calculated) {
-            modified_otel_resource_attributes_value = res_attrs.getModifiedOtelResourceAttributesValue(original_value);
-            modified_otel_resource_attributes_value_calculated = true;
-        }
-        if (modified_otel_resource_attributes_value) |updated_value| {
-            return updated_value;
-        }
-    } else if (std.mem.eql(u8, name, jvm.java_tool_options_env_var_name)) {
+    if (std.mem.eql(u8, name, jvm.java_tool_options_env_var_name)) {
         if (!modified_java_tool_options_value_calculated) {
             modified_java_tool_options_value =
                 jvm.checkOTelJavaAgentJarAndGetModifiedJavaToolOptionsValue(original_value);
