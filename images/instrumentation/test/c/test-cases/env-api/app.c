3dc8d75b5c11ff40175efdeb316bcd8ba8e501e0
Bastian Krol <bastian.krol@dash0.com>
2025-09-29 13:09:54 +0200

    check __environ content in env-api test case

--- images/instrumentation/test/c/test-cases/env-api/app.c
+++ images/instrumentation/test/c/test-cases/env-api/app.c
@@ -1,14 +1,17 @@
 // SPDX-FileCopyrightText: Copyright 2025 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-void expect_getenv_value(char* name, char* expected) {
-  char* actual = getenv(name);
+extern char **__environ;
+
+void expect_getenv_value(char *name, char *expected) {
+  char *actual = getenv(name);
   if (actual == NULL) {
     fprintf(stderr,
             "Unexpected value for the environment variable %s --\nexpected: "
@@ -21,6 +24,59 @@ void expect_getenv_value(char* name, char* expected) {
             "Unexpected value for the environment variable %s --\nexpected: "
             "%s\n     was: %s\n",
             name, expected, actual);
+    exit(1);
+  }
+}
+
+void expect_environ_content(const char *expected_key_value_pairs[], int count) {
+  for (int i = 0; i < count; i++) {
+    const char *expected = expected_key_value_pairs[i];
+    bool found = false;
+    char **env_ptr = __environ;
+    while (*env_ptr) {
+      char *kv_pair_from_environ = *env_ptr;
+      if (strcmp(expected_key_value_pairs[i], kv_pair_from_environ) == 0) {
+        found = true;
+        break;
+      }
+      env_ptr++;
+    }
+    if (!found) {
+      fprintf(stderr,
+              "Expected __environ to contain %s, but it didn't --\n__environ: ",
+              expected);
+      char **env_ptr = __environ;
+      while (*env_ptr) {
+        fprintf(stderr, "%s|", *env_ptr);
+        env_ptr++;
+      }
+      fprintf(stderr, "\n");
+      exit(1);
+    }
+  }
+}
+
+void expect_empty_environ() {
+  int i = 0;
+  char **env_ptr = __environ;
+  if (env_ptr == NULL) {
+    // Looks like musl and glibc implement clearenv differently -- in glibc, __environ is a valid pointer, but has no
+    // entries (i.e. the first element is NULL). In musl, __environ is set to NULL (and probably re-initialized on the
+    // next call to putenv/setenv).
+    return;
+  }
+  while (*env_ptr) {
+	i++;
+    env_ptr++;
+  }
+  if (i != 0) {
+    fprintf(stderr, "Expected empty __environ, but it was not empty");
+    char **env_ptr = __environ;
+    while (*env_ptr) {
+      fprintf(stderr, "%s|", *env_ptr);
+      env_ptr++;
+    }
+    fprintf(stderr, "\n");
     exit(1);
   }
 }
@@ -71,6 +127,18 @@ int main() {
 
   int err = 0;
 
+  expect_environ_content(
+      (const char *[]){
+          "EXISTING_VAR_1=original-value-1",
+          "EXISTING_VAR_2=original-value-2",
+          "EXISTING_VAR_3=original-value-3",
+          "EXISTING_VAR_4=original-value-4",
+          "EXISTING_VAR_5=original-value-5",
+          "EXISTING_VAR_6=original-value-6",
+          "EMPTY_ENV_VAR=",
+      },
+      7);
+
   // This variable is not set, libc is supposed to return NULL.
   expect_getenv_null("NON_EXISTING_VAR_1");
   // This variable is explicitly set to the empty string, getenv is supposed to return "".
@@ -83,32 +151,102 @@ int main() {
   expect_getenv_value("EXISTING_VAR_5", "original-value-5");
   expect_getenv_value("EXISTING_VAR_6", "original-value-6");
 
-  expect_putenv_no_err("EXISTING_VAR_1=new value from putenv for variable that existed before");
-  expect_getenv_value("EXISTING_VAR_1", "new value from putenv for variable that existed before");
+  expect_putenv_no_err(
+      "EXISTING_VAR_1=new value from putenv for variable that existed before");
+  expect_getenv_value("EXISTING_VAR_1",
+                      "new value from putenv for variable that existed before");
+  expect_environ_content(
+      (const char *[]){
+          "EXISTING_VAR_1=new value from putenv for variable that existed before",
+          "EXISTING_VAR_2=original-value-2",
+          "EXISTING_VAR_3=original-value-3",
+          "EXISTING_VAR_4=original-value-4",
+          "EXISTING_VAR_5=original-value-5",
+          "EXISTING_VAR_6=original-value-6",
+          "EMPTY_ENV_VAR=",
+      },
+      7);
 
   expect_putenv_no_err("NON_EXISTING_VAR_1=new value from putenv for variable that did not exist before");
-  expect_getenv_value("NON_EXISTING_VAR_1", "new value from putenv for variable that did not exist before");
+  expect_getenv_value(
+      "NON_EXISTING_VAR_1",
+      "new value from putenv for variable that did not exist before");
+  expect_environ_content(
+      (const char *[]){
+          "EXISTING_VAR_1=new value from putenv for variable that existed before",
+          "EXISTING_VAR_2=original-value-2",
+          "EXISTING_VAR_3=original-value-3",
+          "EXISTING_VAR_4=original-value-4",
+          "EXISTING_VAR_5=original-value-5",
+          "EXISTING_VAR_6=original-value-6",
+          "EMPTY_ENV_VAR=",
+          "NON_EXISTING_VAR_1=new value from putenv for variable that did not exist before",
+      },
+      8);
 
   // Calling putenv with a string without "=" should delete the env var.
-  expect_putenv_no_err("EXISTING_VAR_2"); // TODO this crashes, probably segfault?
+  expect_putenv_no_err("EXISTING_VAR_2");
   expect_getenv_null("EXISTING_VAR_2");
+  expect_environ_content(
+      (const char *[]){
+          "EXISTING_VAR_1=new value from putenv for variable that existed before",
+          "EXISTING_VAR_3=original-value-3",
+          "EXISTING_VAR_4=original-value-4",
+          "EXISTING_VAR_5=original-value-5",
+          "EXISTING_VAR_6=original-value-6",
+          "EMPTY_ENV_VAR=",
+          "NON_EXISTING_VAR_1=new value from putenv for variable that did not exist before",
+      },
+      7);
 
-  setenv("NON_EXISTING_VAR_2", "value from setenv for non-existing without replace", 0);
-  expect_getenv_value("NON_EXISTING_VAR_2", "value from setenv for non-existing without replace");
-  setenv("NON_EXISTING_VAR_3", "value from setenv for non-existing with replace", 1);
-  expect_getenv_value("NON_EXISTING_VAR_3", "value from setenv for non-existing with replace");
+  setenv("NON_EXISTING_VAR_2",
+         "value from setenv for non-existing without replace", 0);
+  expect_getenv_value("NON_EXISTING_VAR_2",
+                      "value from setenv for non-existing without replace");
+  setenv("NON_EXISTING_VAR_3",
+         "value from setenv for non-existing with replace", 1);
+  expect_getenv_value("NON_EXISTING_VAR_3",
+                      "value from setenv for non-existing with replace");
   setenv("EXISTING_VAR_3", "value from setenv for existing without replace", 0);
   expect_getenv_value("EXISTING_VAR_3", "original-value-3");
   setenv("EXISTING_VAR_4", "value from setenv for existing with replace", 1);
-  expect_getenv_value("EXISTING_VAR_4", "value from setenv for existing with replace");
+  expect_getenv_value("EXISTING_VAR_4",
+                      "value from setenv for existing with replace");
   // replace with empty string
   setenv("EXISTING_VAR_4", "", 1);
   expect_getenv_value("EXISTING_VAR_4", "");
+
+  expect_environ_content(
+      (const char *[]){
+          "EXISTING_VAR_1=new value from putenv for variable that existed before",
+          "EXISTING_VAR_3=original-value-3",
+          "EXISTING_VAR_4=",
+          "EXISTING_VAR_5=original-value-5",
+          "EXISTING_VAR_6=original-value-6",
+          "EMPTY_ENV_VAR=",
+          "NON_EXISTING_VAR_1=new value from putenv for variable that did not exist before",
+          "NON_EXISTING_VAR_2=value from setenv for non-existing without replace",
+          "NON_EXISTING_VAR_3=value from setenv for non-existing with replace",
+      },
+      9);
 
   expect_unsetenv_no_err("NON_EXISTING_VAR_4");
   expect_getenv_null("NON_EXISTING_VAR_4");
   expect_unsetenv_no_err("EXISTING_VAR_5");
   expect_getenv_null("EXISTING_VAR_5");
+
+  expect_environ_content(
+      (const char *[]){
+          "EXISTING_VAR_1=new value from putenv for variable that existed before",
+          "EXISTING_VAR_3=original-value-3",
+          "EXISTING_VAR_4=",
+          "EXISTING_VAR_6=original-value-6",
+          "EMPTY_ENV_VAR=",
+          "NON_EXISTING_VAR_1=new value from putenv for variable that did not exist before",
+          "NON_EXISTING_VAR_2=value from setenv for non-existing without replace",
+          "NON_EXISTING_VAR_3=value from setenv for non-existing with replace",
+      },
+      8);
 
   err = unsetenv("string=with_equals_character");
   if (err == 0) {
@@ -121,7 +259,7 @@ int main() {
     fprintf(stderr, "clearenv() failed, return code: %d\n", err);
     exit(1);
   }
-
+  expect_empty_environ();
   expect_getenv_null("EXISTING_VAR_1");
   expect_getenv_null("EXISTING_VAR_2");
   expect_getenv_null("EXISTING_VAR_3");
@@ -137,7 +275,4 @@ int main() {
   expect_putenv_no_err("NEW_VAR=value");
   expect_getenv_value("NEW_VAR", "value");
   expect_getenv_null("DOES_NOT_EXIST");
-
-  // TODO Add additional tests that read environ, _environ, __environ after executing putenv/setenv when continuing with
-  // https://linear.app/dash0/issue/ENG-4882/export-environ-instead-of-getenv-in-injector
 }
