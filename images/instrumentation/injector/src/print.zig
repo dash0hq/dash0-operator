d263649fe84a3b9e62ad262f683e388163289d4b
Bastian Krol <bastian.krol@dash0.com>
2025-10-09 17:19:45 +0200

    chore(injector): reduce log verbosity, add DASH0_INJECTOR_QUIET
    - DASH0_INJECTOR_QUIET=true suppresses all output
    - the default output (when DASH0_INJECTOR_QUIET is not set,
      DASH0_INJECTOR_DEBUG is not set, and no error occurs) is now only
      exactly one line, to indicate that the injector is active

--- images/instrumentation/injector/src/print.zig
+++ images/instrumentation/injector/src/print.zig
@@ -4,14 +4,20 @@
 const std = @import("std");
 
 const dash0_injector_debug_env_var_name = "DASH0_INJECTOR_DEBUG";
+const dash0_injector_quiet_env_var_name = "DASH0_INJECTOR_QUIET";
 const log_prefix = "[Dash0 injector] ";
 
 var is_debug = false;
+var is_quiet = false;
 
-/// Initializes the is_debug flag based on the environment variable DASH0_INJECTOR_DEBUG.
-pub fn initDebugFlag() void {
+/// Initializes the is_debug and the is_quiet flag based on the environment variables DASH0_INJECTOR_DEBUG and
+/// DASH0_INJECTOR_QUIET.
+pub fn initFlags() void {
     if (std.posix.getenv(dash0_injector_debug_env_var_name)) |is_debug_raw| {
         is_debug = std.ascii.eqlIgnoreCase("true", is_debug_raw);
+    }
+    if (std.posix.getenv(dash0_injector_quiet_env_var_name)) |is_quiet_raw| {
+        is_quiet = std.ascii.eqlIgnoreCase("true", is_quiet_raw);
     }
 }
 
@@ -19,12 +25,22 @@ pub fn isDebug() bool {
     return is_debug;
 }
 
+pub fn isQuiet() bool {
+    return is_quiet;
+}
+
 pub fn printDebug(comptime fmt: []const u8, args: anytype) void {
     if (is_debug) {
-        std.debug.print(log_prefix ++ fmt ++ "\n", args);
+        _printMessage(fmt, args);
     }
 }
 
 pub fn printMessage(comptime fmt: []const u8, args: anytype) void {
+    if (!is_quiet or is_debug) { // DASH0_INJECTOR_DEBUG overrides DASH0_INJECTOR_QUIET
+        _printMessage(fmt, args);
+    }
+}
+
+fn _printMessage(comptime fmt: []const u8, args: anytype) void {
     std.debug.print(log_prefix ++ fmt ++ "\n", args);
 }
