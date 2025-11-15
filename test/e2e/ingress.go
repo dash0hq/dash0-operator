57be6c551b06ae0d732b62c6b6df7ebca9b6a5d0
Bastian Krol <bastian.krol@dash0.com>
2025-11-15 11:29:43 +0100

    test(e2e): deploy/remove ingress-nginx on demand
    This is a follow up to 24843ee0c79bde4474296afc5f65d46e0eff9d80,
    aligning the e2e test suite with the test scripts.
    
    In particular:
    * If the e2e test suite startup finds that ingress-nginx is already
      installed, it does nothing (won't install/change it, will not
      uninstall it at the end)
    * If the e2e test suite startup finds that ingress-nginx is not
      installed, it deploys it.
    * If the e2e test suite has installed ingress-nginx, it will also remove
      it at the end -- this notably differs from the test scripts (which
      deliberately leave ingress-nginx installed on cleanup, since it is
      considered basic infrastructure). For the e2e test suite, is in line
      with how we handle metrics-server and cert-manager, removing it in the
      teardown logic only if it has been installed.

--- test/e2e/ingress.go
+++ test/e2e/ingress.go
@@ -4,21 +4,48 @@
 package e2e
 
 import (
+	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-func deployIngressController(cleanupSteps *neccessaryCleanupSteps) {
+func ensureNginxIngressControllerIsInstalled(cleanupSteps *neccessaryCleanupSteps) {
+	if os.Getenv("DEPLOY_NGINX_INGRESS") == "false" {
+		return
+	}
+	By("checking if ingress-nginx is already running")
+	if err := runAndIgnoreOutput(
+		exec.Command(
+			"kubectl",
+			"wait",
+			"--namespace",
+			"ingress-nginx",
+			"--for=condition=ready",
+			"pod",
+			"--selector=app.kubernetes.io/component=controller",
+			"--timeout=5s",
+		)); err == nil {
+		e2ePrint("ingress-nginx is already running\n")
+		return
+	}
+
+	e2ePrint(
+		"Hint: To get a faster feedback cycle on e2e tests, deploy the ingress-nginx once via the following commands:\n" +
+			"  kubectl apply -k test-resources/nginx\n" +
+			"If the e2e tests find an existing ingress-nginx installation, they will not deploy the ingress-nginx and " +
+			"they will also not undeploy it after running the test suite.\n",
+	)
+	installNginxIngressController(cleanupSteps)
+}
+
+func installNginxIngressController(cleanupSteps *neccessaryCleanupSteps) {
 	By("deploying ingress-nginx")
-	Expect(runAndIgnoreOutput(exec.Command(
-		"kubectl",
-		"apply",
-		"-f",
-		"https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml",
-	))).To(Succeed())
 	cleanupSteps.removeIngressNginx = true
+	Expect(runAndIgnoreOutput(exec.Command(
+		"kubectl", "apply", "-k", "test-resources/nginx",
+	))).To(Succeed())
 
 	By("waiting for ingress-nginx")
 	Expect(runAndIgnoreOutput(exec.Command(
@@ -39,10 +66,6 @@ func undeployNginxIngressController(cleanupSteps *neccessaryCleanupSteps) {
 	}
 	By("removing nginx ingress controller")
 	Expect(runAndIgnoreOutput(exec.Command(
-		"kubectl",
-		"delete",
-		"-f",
-		"https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml",
-		"--ignore-not-found",
+		"kubectl", "delete", "-k", "test-resources/nginx", "--ignore-not-found",
 	))).To(Succeed())
 }
