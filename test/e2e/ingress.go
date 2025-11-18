99f14532a2a1c32dfdc11a94a1565ace6c015205
Bastian Krol <bastian.krol@dash0.com>
2025-11-18 10:44:57 +0100

    test(e2e): wait for ingress-nginx admission webhook endpoint

--- test/e2e/ingress.go
+++ test/e2e/ingress.go
@@ -6,6 +6,7 @@ package e2e
 import (
 	"os"
 	"os/exec"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -58,6 +59,48 @@ func installNginxIngressController(cleanupSteps *neccessaryCleanupSteps) {
 		"--selector=app.kubernetes.io/component=controller",
 		"--timeout=90s",
 	))).To(Succeed())
+
+	// The ingress-nginx pod becoming ready (checked above) is not sufficient for being able to continue. We also need
+	// to wait for the ingress-nginx-controller-admission webhook service to be ready and reachable. Otherwise, when
+	// deploying right after installing the ingress-nginx, we might run into
+	//   Internal error occurred: failed calling webhook "validate.nginx.ingress.kubernetes.io": failed to call webhook:
+	//   Post "https://ingress-nginx-controller-admission.ingress-nginx.svc:443/networking/v1/ingresses?timeout=10s":
+	//   dial tcp ...:443: connect: connection refused
+	// Hence, we also wait until the endpointslice for this webhook service becomes ready and has a port assigned.
+	Eventually(func(g Gomega) {
+		g.Expect(runAndIgnoreOutput(exec.Command(
+			"kubectl",
+			"get",
+			"--namespace",
+			"ingress-nginx",
+			"service",
+			"ingress-nginx-controller-admission",
+		))).To(Succeed())
+		endpointSliceReady, err := run(exec.Command(
+			"kubectl",
+			"get",
+			"--namespace",
+			"ingress-nginx",
+			"endpointslice",
+			"-l",
+			"kubernetes.io/service-name=ingress-nginx-controller-admission",
+			"-o=jsonpath='{.items[0].endpoints[0].conditions.ready}'",
+		))
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(endpointSliceReady).To(Equal("'true'"))
+		endpointSlicePort, err := run(exec.Command(
+			"kubectl",
+			"get",
+			"--namespace",
+			"ingress-nginx",
+			"endpointslice",
+			"-l",
+			"kubernetes.io/service-name=ingress-nginx-controller-admission",
+			"-o=jsonpath='{.items[0].ports[0].port}'",
+		))
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(endpointSlicePort).To(Equal("'8443'"))
+	}, 60*time.Second, 1*time.Second).Should(Succeed())
 }
 
 func undeployNginxIngressController(cleanupSteps *neccessaryCleanupSteps) {
