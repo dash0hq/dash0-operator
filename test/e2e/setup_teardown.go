e104b2ad7868e4d186cc089b5801591f557423b3
Bastian Krol <bastian.krol@dash0.com>
2025-11-18 10:08:57 +0100

    test(e2e): remove outdated check for required ports

--- test/e2e/setup_teardown.go
+++ test/e2e/setup_teardown.go
@@ -6,7 +6,6 @@ package e2e
 import (
 	"fmt"
 	"os/exec"
-	"strconv"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -16,61 +15,7 @@ import (
 var (
 	originalKubeContext       string
 	kubeContextHasBeenChanged bool
-
-	requiredPorts = []int{
-		// Node.js workloads
-		1205, 1206, 1207, 1208, 1209, 1210, 1211, //
-		// JVM workloads
-		1306, 1307, 1309, 1310, 1311, //
-		// .NET workloads
-		1406, 1407, 1409, 1410, 1411, //
-		// Python workloads
-		1506, 1507, 1509, 1510, 1511, //
-		// OTel Collector
-		4317, 4318,
-		// Dash0 API mock server
-		8001,
-	}
 )
-
-func checkIfRequiredPortsAreBlocked() {
-	portsCurrentlyInUseByKubernetesServices, err := run(
-		exec.Command(
-			"kubectl",
-			"get",
-			"svc",
-			"--all-namespaces",
-			"-o",
-			"go-template='{{range .items}}{{range.spec.ports}}{{if .port}}{{.port}}{{\"\\n\"}}{{end}}{{end}}{{end}}'",
-		))
-	Expect(err).NotTo(HaveOccurred())
-	portsCurrentlyInUseArray := getNonEmptyLines(portsCurrentlyInUseByKubernetesServices)
-	messages := make([]string, 0)
-	foundBlockedPort := false
-	for _, usedPortStr := range portsCurrentlyInUseArray {
-		usedPort, err := strconv.Atoi(usedPortStr)
-		if err != nil {
-			continue
-		}
-		for _, requiredPort := range requiredPorts {
-			if usedPort == requiredPort {
-				messages = append(messages,
-					fmt.Sprintf(
-						"Port %d is required by the test suite, but it is already in use by a Kubernetes "+
-							"service. Please check for conflicting deployed serivces.",
-						requiredPort,
-					))
-				foundBlockedPort = true
-			}
-		}
-	}
-	if foundBlockedPort {
-		messages = append(messages,
-			"Note: If you have used the scripts for manual testing in test-resources, running "+
-				"test-resources/bin/test-cleanup.sh might help removing all left-over Kubernetes objects.")
-		Fail(strings.Join(messages, "\n"))
-	}
-}
 
 func setKubernetesContext(kubernetesContextForTest string) (bool, string) {
 	By("reading current Kubernetes context")
