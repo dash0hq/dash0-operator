a564b775f42f82511b2e260f5c5a61b266ac03ed
Bastian Krol <bastian.krol@dash0.com>
2025-11-21 12:16:20 +0100

    test(test-resources): add dedicated scripts to deploy/delete ingress-nginx
    This is slightly advantageous over calling kubect apply/delete directly,
    due to including verify_kubectx in the mix.

---
+++ test-resources/bin/delete-ingress-nginx.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright 2025 Dash0 Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+project_root="$(dirname "${BASH_SOURCE[0]}")"/../..
+scripts_lib="test-resources/bin/lib"
+
+cd "$project_root"
+
+# shellcheck source=./lib/constants
+source "$scripts_lib/constants"
+
+# shellcheck source=./lib/util
+source "$scripts_lib/util"
+
+load_env_file
+
+# The main reason for this even being a script instead of just using `kubectl delete -k test-resources/nginx` directly
+# is verify_kubectx, which offers protection against accidentally making changes in production clusters.
+verify_kubectx
+
+kubectl delete -k test-resources/nginx
