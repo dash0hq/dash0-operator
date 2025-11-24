33222b6e927de129670f8deccf72c1df36beb53a
Thiemo Krause <thiemo.krause@dash0.com>
2025-11-24 14:08:05 +0100

    feat(collectors): only collect events for enabled namespaces

--- internal/collectors/otelcolresources/collector_config_maps.go
+++ internal/collectors/otelcolresources/collector_config_maps.go
@@ -83,6 +83,7 @@ type collectorConfigurationTemplateValues struct {
 	IsGkeAutopilot                                   bool
 	PseudoClusterUid                                 string
 	ClusterName                                      string
+	NamespacesWithMonitoring                         []string
 	NamespacesWithLogCollection                      []string
 	NamespaceOttlFilter                              string
 	NamespacesWithPrometheusScraping                 []string
@@ -220,6 +221,7 @@ func assembleCollectorConfigMap(
 				IsGkeAutopilot:                                   config.IsGkeAutopilot,
 				PseudoClusterUid:                                 string(config.PseudoClusterUid),
 				ClusterName:                                      config.ClusterName,
+				NamespacesWithMonitoring:                         monitoredNamespaces,
 				NamespacesWithLogCollection:                      namespacesWithLogCollection,
 				NamespaceOttlFilter:                              namespaceOttlFilter,
 				NamespacesWithPrometheusScraping:                 namespacesWithPrometheusScraping,
